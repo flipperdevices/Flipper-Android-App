ccc253fa536b29c70f03619ce2996f00fea0b27a
rougsig <rougsig@gmail.com>
2020-08-18 22:56:39 +0200

    Add ConfigurationPlugin

---
+++ buildSrc/src/main/java/com/flipper/gradle/LibraryExtensionConfiguration.kt
@@ -0,0 +1,32 @@
+package com.flipper.gradle
+
+import ApkConfig
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Project
+
+fun LibraryExtension.configure(project: Project) {
+    configureDefaultConfig()
+    configureBuildTypes()
+}
+
+private fun LibraryExtension.configureDefaultConfig() {
+    compileSdkVersion(ApkConfig.COMPILE_SDK_VERSION)
+    defaultConfig.setMinSdkVersion(ApkConfig.MIN_SDK_VERSION)
+    defaultConfig.setTargetSdkVersion(ApkConfig.TARGET_SDK_VERSION)
+    defaultConfig.consumerProguardFiles("consumer-rules.pro")
+    defaultConfig.setVersionCode(ApkConfig.VERSION_CODE)
+    defaultConfig.setVersionName(ApkConfig.VERSION_NAME)
+}
+
+private fun LibraryExtension.configureBuildTypes() {
+    buildTypes { container ->
+        container.maybeCreate("debug")
+        container.maybeCreate("release").apply {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
