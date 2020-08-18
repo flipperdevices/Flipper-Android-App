299eb7459771d3d51a6c299e78de4f3b5905ea52
Andrey Stepankov <rougsig@gmail.com>
2020-08-18 22:20:17 +0200

    Add buildSrc Dependencies (#5)

---
+++ buildSrc/src/main/kotlin/ApkConfig.kt
@@ -0,0 +1,7 @@
+object ApkConfig {
+    const val APPLICATION_ID = "com.flipper.app"
+
+    const val MIN_SDK_VERSION = 21
+    const val TARGET_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 30
+}
