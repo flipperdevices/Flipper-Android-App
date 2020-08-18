a46ec11c9cf7971c7337724e304b609396d972b4
LionZXY <nikita@kulikof.ru>
2020-08-18 14:19:44 +0300

    Add timber to deps (#3)

---
+++ app/src/main/java/com/flipper/app/App.kt
@@ -0,0 +1,15 @@
+package com.flipper.app
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        Timber.v("Started!")
+    }
+}
