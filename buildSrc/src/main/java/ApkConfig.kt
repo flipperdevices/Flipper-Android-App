854f89877edeaf6d80fc76629565176df6f25a99
Andrey Stepankov <rougsig@gmail.com>
2020-08-19 18:01:07 +0200

    Add ConfigurationPlugin (#6)

--- buildSrc/src/main/java/ApkConfig.kt
+++ buildSrc/src/main/java/ApkConfig.kt
@@ -4,4 +4,7 @@ object ApkConfig {
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 30
     const val COMPILE_SDK_VERSION = 30
+
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
 }
