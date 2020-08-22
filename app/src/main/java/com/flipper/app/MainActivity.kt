7797eb180f169de946f1bce5077d7432b3e8f4d1
rougsig <rougsig@gmail.com>
2020-08-22 13:15:54 +0200

    Enable databinding

--- app/src/main/java/com/flipper/app/MainActivity.kt
+++ app/src/main/java/com/flipper/app/MainActivity.kt
@@ -2,10 +2,12 @@ package com.flipper.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.flipper.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    val binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }
