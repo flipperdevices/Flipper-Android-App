710312da4f66f709cc8419d143bdf98f981b2aca
rougsig <rougsig@gmail.com>
2020-08-24 15:09:18 +0200

    Add BaseController

---
+++ core/src/main/java/com/flipper/core/view/BaseController.kt
@@ -0,0 +1,40 @@
+package com.flipper.core.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.bluelinelabs.conductor.Controller
+
+abstract class BaseController<VB : ViewBinding> : Controller {
+  private var _binding: VB? = null
+  protected val binding: VB
+    get() = _binding
+      ?: error("attempt to get binding before onCreateView or after onDestroyView")
+
+  constructor()
+  constructor(args: Bundle) : super(args)
+
+  final override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup,
+    savedViewState: Bundle?
+  ): View {
+    _binding = getViewInflater().invoke(inflater, container, false)
+    val rootView = _binding!!.root
+    initializeView()
+    return rootView
+  }
+
+  abstract fun getViewInflater(): ViewInflater<VB>
+
+  abstract fun initializeView()
+  protected open fun disposeView() = Unit
+
+  final override fun onDestroyView(view: View) {
+    disposeView()
+    _binding = null
+    super.onDestroyView(view)
+  }
+}
