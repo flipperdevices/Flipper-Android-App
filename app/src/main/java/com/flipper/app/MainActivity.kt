345ca7fe9d70f23b75b7821e98ad86371dadef2e
rougsig <rougsig@gmail.com>
2020-08-24 14:19:46 +0200

    Reformat project
    Change indents size from 4 spaces to 2 spaces

--- app/src/main/java/com/flipper/app/MainActivity.kt
+++ app/src/main/java/com/flipper/app/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme)
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    setTheme(R.style.AppTheme)
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 }
