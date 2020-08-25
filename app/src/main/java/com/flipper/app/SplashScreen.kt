ff360abe5a1738c104d2651364510d43382a6396
Nuru Nabiyev <nuru.nabiyev@gmail.com>
2020-08-25 21:42:55 +0200

    [fix-ui] finish SplashScreen.kt

--- app/src/main/java/com/flipper/app/SplashScreen.kt
+++ app/src/main/java/com/flipper/app/SplashScreen.kt
@@ -8,5 +8,6 @@ class SplashScreen : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     startActivity(Intent(this, MainActivity::class.java))
+    finish()
   }
 }
