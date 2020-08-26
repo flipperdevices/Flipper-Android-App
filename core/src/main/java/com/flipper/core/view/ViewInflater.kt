3fcf0ad8ce1a2b7f8dffa61ab53bb83a2fa39eba
rougsig <rougsig@gmail.com>
2020-08-26 19:42:27 +0200

    Add names to parameters in ViewInflater typealias

--- core/src/main/java/com/flipper/core/view/ViewInflater.kt
+++ core/src/main/java/com/flipper/core/view/ViewInflater.kt
@@ -3,4 +3,4 @@ package com.flipper.core.view
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
-typealias ViewInflater<T> = (LayoutInflater, ViewGroup, Boolean) -> T
+typealias ViewInflater<T> = (LayoutInflater, parent: ViewGroup, attachToParent: Boolean) -> T
