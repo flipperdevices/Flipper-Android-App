6d74efedb17b10f7e83b44b389fc0a1fc9288a6b
rougsig <rougsig@gmail.com>
2020-08-31 19:06:46 +0200

    Rename App to FlipperApplication

--- app/src/main/java/com/flipper/app/FlipperApplication.kt
+++ app/src/main/java/com/flipper/app/FlipperApplication.kt
@@ -3,7 +3,7 @@ package com.flipper.app
 import android.app.Application
 import timber.log.Timber
 
-class App : Application() {
+class FlipperApplication : Application() {
   override fun onCreate() {
     super.onCreate()
     if (BuildConfig.DEBUG) {
