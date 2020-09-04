00003d2bcefbb0b421ef6c6e33c2387b8896e1e8
lionzxy <nikita@kulikof.ru>
2020-09-04 21:19:46 +0300

    Fix ktlint error

--- bridge/build.gradle.kts
+++ bridge/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
-  id("com.android.library")
-  id("kotlin-android")
+    id("com.android.library")
+    id("kotlin-android")
 }
 
 apply<com.flipper.gradle.ConfigurationPlugin>()
 
 dependencies {
-  implementation(Libs.TIMBER)
-  implementation(Libs.KOTLIN)
-  implementation(Libs.ANNOTATIONS)
-  implementation(Libs.CORE_KTX)
-  implementation(Libs.APPCOMPAT)
+    implementation(Libs.TIMBER)
+    implementation(Libs.KOTLIN)
+    implementation(Libs.ANNOTATIONS)
+    implementation(Libs.CORE_KTX)
+    implementation(Libs.APPCOMPAT)
 
-  testImplementation(TestingLib.JUNIT)
-  androidTestImplementation(TestingLib.ANDROIDX_TEST_EXT_JUNIT)
-  androidTestImplementation(TestingLib.ESPRESSO_CORE)
+    testImplementation(TestingLib.JUNIT)
+    androidTestImplementation(TestingLib.ANDROIDX_TEST_EXT_JUNIT)
+    androidTestImplementation(TestingLib.ESPRESSO_CORE)
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,22 +1,22 @@
 plugins {
-  id("com.android.library")
-  id("kotlin-android")
+    id("com.android.library")
+    id("kotlin-android")
 }
 
 apply<com.flipper.gradle.ConfigurationPlugin>()
 
 dependencies {
-  api(Libs.CONDUCTOR)
+    api(Libs.CONDUCTOR)
 
-  implementation(Libs.TIMBER)
-  implementation(Libs.KOTLIN)
-  implementation(Libs.ANNOTATIONS)
-  implementation(Libs.CORE_KTX)
-  implementation(Libs.APPCOMPAT)
-  implementation(Libs.DAGGER)
-  implementation(Libs.MOXY)
+    implementation(Libs.TIMBER)
+    implementation(Libs.KOTLIN)
+    implementation(Libs.ANNOTATIONS)
+    implementation(Libs.CORE_KTX)
+    implementation(Libs.APPCOMPAT)
+    implementation(Libs.DAGGER)
+    implementation(Libs.MOXY)
 
-  testImplementation(TestingLib.JUNIT)
-  androidTestImplementation(TestingLib.ANDROIDX_TEST_EXT_JUNIT)
-  androidTestImplementation(TestingLib.ESPRESSO_CORE)
+    testImplementation(TestingLib.JUNIT)
+    androidTestImplementation(TestingLib.ANDROIDX_TEST_EXT_JUNIT)
+    androidTestImplementation(TestingLib.ESPRESSO_CORE)
 }
