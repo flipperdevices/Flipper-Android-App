9024d21f111afbe69a5460e4e33c5fe66f7181f2
rougsig <rougsig@gmail.com>
2020-09-06 14:41:36 +0200

    Fix moxy lifecycle in BaseController

--- core/src/main/java/com/flipper/core/view/BaseController.kt
+++ core/src/main/java/com/flipper/core/view/BaseController.kt
@@ -12,25 +12,26 @@ import moxy.MvpDelegate
 abstract class BaseController<VB : ViewBinding> : Controller {
     // Lazy used for prevent leaking `this`
     private val mvpDelegate by lazy(LazyThreadSafetyMode.NONE) { MvpDelegate(this) }
+    private var isStateSaved = false
+    private var isCreated = false
 
     private var _binding: VB? = null
     protected val binding: VB
         get() = _binding
             ?: error("attempt to get binding before onCreateView or after onDestroyView")
 
-    constructor() {
-        mvpDelegate.onCreate()
-    }
-
-    constructor(args: Bundle) : super(args) {
-        mvpDelegate.onCreate(args)
-    }
+    constructor()
+    constructor(args: Bundle) : super(args)
 
     final override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup,
         savedViewState: Bundle?
     ): View {
+        if (!isCreated) {
+            isCreated = true
+            mvpDelegate.onCreate()
+        }
         _binding = getViewInflater().invoke(inflater, container, false)
         val rootView = _binding!!.root
         initializeView()
@@ -57,11 +58,21 @@ abstract class BaseController<VB : ViewBinding> : Controller {
     }
 
     final override fun onDestroy() {
-        mvpDelegate.onDetach()
+        // If state is saved, it means controller rotation.
+        // Do not destroy presenter.
+        if (isStateSaved) return
+        mvpDelegate.onDestroy()
     }
 
     @CallSuper
     override fun onSaveInstanceState(outState: Bundle) {
+        isStateSaved = true
         mvpDelegate.onSaveInstanceState(outState)
+    }
+
+    @CallSuper
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        isCreated = true
+        mvpDelegate.onCreate(savedInstanceState)
     }
 }
