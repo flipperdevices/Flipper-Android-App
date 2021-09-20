68eb40ab4669794c88684945536d980a10382191
LionZXY <nikita@kulikof.ru>
2021-09-20 17:57:03 +0300

    Remove pass pair screen if we have saved device

--- components/pair/src/main/java/com/flipper/pair/find/FindDeviceFragment.kt
+++ components/pair/src/main/java/com/flipper/pair/find/FindDeviceFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.compose.runtime.Composable
 import androidx.core.content.edit
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.flipper.bridge.api.scanner.DiscoveredBluetoothDevice
 import com.flipper.core.api.BottomNavigationActivityApi
 import com.flipper.core.di.ComponentHolder
@@ -16,8 +15,6 @@ import com.flipper.pair.di.PairComponent
 import com.flipper.pair.find.compose.ComposeFindDevice
 import com.flipper.pair.find.service.BLEDeviceViewModel
 import com.github.terrakok.cicerone.Router
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class FindDeviceFragment : ComposeFragment() {
@@ -48,16 +45,6 @@ class FindDeviceFragment : ComposeFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bleDeviceViewModel.startScanIfNotYet()
-        val deviceId = sharedPreferences.getString(FlipperSharedPreferencesKey.DEVICE_ID, null)
-        if (deviceId != null) {
-            lifecycleScope.launch {
-                bleDeviceViewModel.state.collect { deviceList ->
-                    val selectedDevice =
-                        deviceList.find { it.address == deviceId } ?: return@collect
-                    onDeviceSelected(selectedDevice)
-                }
-            }
-        }
     }
 
     private fun onDeviceSelected(discoveredBluetoothDevice: DiscoveredBluetoothDevice) {
