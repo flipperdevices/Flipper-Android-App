2aec2aba6962a7deb7074722039908923015da4d
LionZXY <nikita@kulikof.ru>
2021-09-20 16:53:41 +0300

    Add error toast

--- components/info/src/main/java/com/flipper/info/main/service/FlipperViewModel.kt
+++ components/info/src/main/java/com/flipper/info/main/service/FlipperViewModel.kt
@@ -7,15 +7,20 @@ import com.flipper.bridge.api.FlipperApi
 import com.flipper.bridge.api.device.FlipperDeviceApi
 import com.flipper.bridge.impl.manager.FlipperBleManager
 import com.flipper.bridge.model.FlipperGATTInformation
+import com.flipper.core.utils.toast
+import com.flipper.info.R
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import no.nordicsemi.android.ble.exception.BluetoothDisabledException
 import no.nordicsemi.android.ble.ktx.state.ConnectionState
 import no.nordicsemi.android.ble.ktx.stateAsFlow
+import timber.log.Timber
 
 class FlipperViewModel(application: Application) : AndroidViewModel(application) {
     private val context = application
@@ -45,7 +50,21 @@ class FlipperViewModel(application: Application) : AndroidViewModel(application)
         async { subscribeToEcho(bleManager) }
         async { subscribeToInformationState(bleManager) }
         async { subscribeToConnectionState(bleManager) }
-        FlipperApi.flipperPairApi.connect(context, currentDevice!!)
+        try {
+            FlipperApi.flipperPairApi.connect(context, currentDevice!!)
+        } catch (securityException: SecurityException) {
+            context.toast(R.string.info_pair_err_permission)
+            Timber.e(securityException)
+        } catch (bleDisabled: BluetoothDisabledException) {
+            context.toast(R.string.info_pair_err_ble_disabled)
+            Timber.e(bleDisabled)
+        } catch (timeout: TimeoutCancellationException) {
+            context.toast(R.string.info_pair_err_timeout)
+            Timber.e(timeout)
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            context.toast(R.string.info_pair_err_not_bounded)
+            Timber.e(illegalArgumentException)
+        }
     }
 
     private suspend fun subscribeToEcho(bleManager: FlipperBleManager) =
