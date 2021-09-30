1f000bdb93bbf3b16a999a8d2556630dcd4066cd
LionZXY <nikita@kulikof.ru>
2021-09-30 11:11:04 +0300

    Make shitfix

--- components/pair/impl/src/main/java/com/flipper/pair/impl/navigation/storage/PairStateStorageImpl.kt
+++ components/pair/impl/src/main/java/com/flipper/pair/impl/navigation/storage/PairStateStorageImpl.kt
@@ -29,7 +29,7 @@ class PairStateStorageImpl @Inject constructor(
             tosAccepted = tosAccepted,
             devicePaired = devicePaired,
             permissionGranted = permissionGranted,
-            guidePassed = !devicePaired
+            guidePassed = true // Temporary disabled
         )
     }
 
