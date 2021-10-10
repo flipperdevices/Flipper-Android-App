06696624ff4ed1d24dd91535da33fef22fe51818
LionZXY <nikita@kulikof.ru>
2021-10-10 22:11:09 +0300

    [WIP] Add not worked protobuf module

---
+++ components/bridge/protobuf/build.gradle.kts
@@ -0,0 +1,46 @@
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.id
+import com.google.protobuf.gradle.ofSourceSet
+import com.google.protobuf.gradle.proto
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.google.protobuf")
+}
+apply<com.flipper.gradle.ConfigurationPlugin>()
+
+dependencies {
+    implementation(project(":components:core"))
+    implementation(Libs.PROTOBUF_JAVA)
+}
+
+protobuf {
+    protoc {
+        artifact = Libs.PROTOBUF_PROTOC
+    }
+
+    generatedFilesBaseDir = "$projectDir/build/protobuf"
+
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.builtins {
+                remove("java")
+            }
+            it.plugins {
+                id("javalite")
+            }
+        }
+    }
+}
+
+android {
+    sourceSets {
+        (getByName("main") as com.android.build.gradle.api.AndroidSourceSet).proto {
+            srcDir("${projectDir}/flipperzero-protobuf")
+            outputDir = file("build/protobuf")
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "Flipper App"
 include(":components:bridge:api")
 include(":components:bridge:impl")
 include(":components:bridge:provider")
+include(":components:bridge:protobuf")
 include(":components:core")
 include(":components:pair:api")
 include(":components:pair:impl")
