502ae052af2ceb153225cb40f694c325aa176476
LionZXY <nikita@kulikof.ru>
2021-10-27 17:26:36 +0300

    Update kotlin and compose

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     // Be careful! See more in /buildSrc/src/main/java/Dependencies.kt#NOTE_CONFIGURATION_PLUGIN
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation("com.android.tools.build:gradle:7.0.2")
     implementation("com.squareup.anvil:gradle-plugin:2.3.4")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
