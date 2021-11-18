fce2b01278ffff4ecc592badbe5e0a5da67cba36
LionZXY <nikita@kulikof.ru>
2021-11-18 11:54:04 +0300

    Hotfix release build

--- components/analytics/shake2report/noop/src/main/java/com/flipperdevices/shake2report/noop/Shake2ReportStub.kt
+++ components/analytics/shake2report/noop/src/main/java/com/flipperdevices/shake2report/noop/Shake2ReportStub.kt
@@ -4,9 +4,10 @@ import android.app.Application
 import com.flipperdevices.core.di.AppGraph
 import com.flipperdevices.shake2report.api.Shake2ReportApi
 import com.squareup.anvil.annotations.ContributesBinding
+import javax.inject.Inject
 
 @ContributesBinding(AppGraph::class)
-class Shake2ReportStub : Shake2ReportApi {
+class Shake2ReportStub @Inject constructor() : Shake2ReportApi {
     override fun init(application: Application) {
         // Do nothing
     }
