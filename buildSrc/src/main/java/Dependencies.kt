927eeb906460d1cebfa0994a6bc2e82bf3e987dd
LionZXY <nikita@kulikof.ru>
2022-01-19 15:56:00 +0300

    Revert "Ktlint runtime"
    This reverts commit fb768ca0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,6 +2,10 @@
 // when updating this dependency version, also update them in buildSrc/build.gradle.kts
 // (manual update is required, because this file is not seen by build scripts in buildSrc)
 
+object Versions {
+    const val KTLINT = "0.44.0-SNAPSHOT"
+}
+
 object GradlePlugins {
     const val DETEKT = "1.19.0"
     const val KTLINT = "10.2.0"
