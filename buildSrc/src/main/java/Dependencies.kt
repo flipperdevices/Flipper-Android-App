6823ded0e092d56e446e9decdcd24b96241d5d23
LionZXY <nikita@kulikof.ru>
2022-01-19 16:01:10 +0300

    Add javadoc

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,11 +1,11 @@
-// NOTE_CONFIGURATION_PLUGIN
-// when updating this dependency version, also update them in buildSrc/build.gradle.kts
-// (manual update is required, because this file is not seen by build scripts in buildSrc)
-
 object Versions {
     const val KTLINT = "0.44.0-SNAPSHOT"
 }
 
+/**
+ * We cannot use the version catalog for plugin versions at this time. This will change in the future.
+ * https://melix.github.io/blog/2021/03/version-catalogs-faq.html#_can_i_use_a_version_catalog_to_declare_plugin_versions
+ */
 object GradlePlugins {
     const val DETEKT = "1.19.0"
     const val KTLINT = "10.2.0"
