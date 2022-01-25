dd5fa606f930be0305e48175ebc5158eb0efa4c2
LionZXY <nikita@kulikof.ru>
2022-01-25 09:26:57 +0300

    Remove unusage code

--- components/pair/impl/src/main/java/com/flipperdevices/pair/impl/composable/tos/ComposeTOS.kt
+++ components/pair/impl/src/main/java/com/flipperdevices/pair/impl/composable/tos/ComposeTOS.kt
@@ -1,13 +1,11 @@
 package com.flipperdevices.pair.impl.composable.tos
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -15,18 +13,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -83,79 +73,7 @@ fun TOSContent() {
             text = stringResource(R.string.pair_tos_description),
             fontSize = 14.sp
         )
-        // TOSFooter()
     }
-}
-
-@Composable
-private fun TOSPoint(@StringRes stringResId: Int) {
-    Text(
-        text = "• ${stringResource(id = stringResId)}",
-        color = Color.Black,
-        fontSize = 14.sp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 16.dp)
-    )
-}
-
-private const val STRING_TAG_URL = "URL"
-
-@Composable
-private fun TOSFooter() {
-    val annotatedString = buildAnnotatedString {
-        append(stringResource(R.string.pair_tos_footer_full_1))
-        addLink(
-            R.string.pair_tos_footer_user_agreement,
-            R.string.pair_tos_footer_user_agreement_link
-        )
-        append(stringResource(R.string.pair_tos_footer_full_2))
-        addLink(
-            R.string.pair_tos_footer_privacy_policy,
-            R.string.pair_tos_footer_privacy_policy_link
-        )
-        append(stringResource(R.string.pair_tos_footer_full_3))
-    }
-
-    val uriHandler = LocalUriHandler.current
-
-    ClickableText(
-        text = annotatedString,
-        onClick = { position ->
-            val annotation =
-                annotatedString.getStringAnnotations(STRING_TAG_URL, position, position)
-            annotation.firstOrNull()?.let {
-                uriHandler.openUri(it.item)
-            }
-        },
-        style = TextStyle(
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp)
-    )
-}
-
-@Composable
-private fun AnnotatedString.Builder.addLink(
-    @StringRes textResId: Int,
-    @StringRes linkResId: Int
-) {
-    pushStringAnnotation(
-        tag = STRING_TAG_URL,
-        annotation = stringResource(linkResId)
-    )
-    withStyle(
-        style = SpanStyle(
-            textDecoration = TextDecoration.Underline,
-            color = Color.Blue
-        )
-    ) {
-        append(stringResource(textResId))
-    }
-    pop()
 }
 
 @Composable
