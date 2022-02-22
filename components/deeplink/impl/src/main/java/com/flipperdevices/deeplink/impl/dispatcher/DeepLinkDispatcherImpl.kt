db358f4461850a6e31af00f3aa26abf471a76742
LionZXY <nikita@kulikof.ru>
2022-02-22 12:54:54 +0300

    Add logs in DeepLinkDispatcher

--- components/deeplink/impl/src/main/java/com/flipperdevices/deeplink/impl/dispatcher/DeepLinkDispatcherImpl.kt
+++ components/deeplink/impl/src/main/java/com/flipperdevices/deeplink/impl/dispatcher/DeepLinkDispatcherImpl.kt
@@ -1,6 +1,8 @@
 package com.flipperdevices.deeplink.impl.dispatcher
 
 import com.flipperdevices.core.di.AppGraph
+import com.flipperdevices.core.log.LogTagProvider
+import com.flipperdevices.core.log.info
 import com.flipperdevices.deeplink.api.DeepLinkDispatcher
 import com.flipperdevices.deeplink.api.DeepLinkHandler
 import com.flipperdevices.deeplink.model.Deeplink
@@ -10,20 +12,31 @@ import com.github.terrakok.cicerone.Router
 import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
 
-@ContributesBinding(AppGraph::class)
+@ContributesBinding(AppGraph::class, DeepLinkDispatcher::class)
 class DeepLinkDispatcherImpl @Inject constructor(
     fileManagerHandler: FileManagerDeepLinkHandler,
     keyReceiveHandler: ArchiveKeyDeeplinkHandler
-) : DeepLinkDispatcher {
+) : DeepLinkDispatcher, LogTagProvider {
+    override val TAG = "DeepLinkDispatcher"
+
     private val handlers = mutableListOf(
         keyReceiveHandler,
         fileManagerHandler
     )
 
     override fun process(router: Router, deeplink: Deeplink): Boolean {
-        val processHandler = handlers.map { it.isSupportLink(deeplink) to it }
+        val supportedHandlers = handlers.map { it.isSupportLink(deeplink) to it }
             .filter { it.first != null }
+
+        info { "Found ${supportedHandlers.size} supported handlers: $supportedHandlers" }
+
+        val processHandler = supportedHandlers
             .maxByOrNull { it.first!! } ?: return false
+
+        info {
+            "Choice handler ${processHandler.second.javaClass} " +
+                "with priority ${processHandler.first}"
+        }
 
         processHandler.second.processLink(router, deeplink)
 
