742957e6fdee20049974a61302e68c8fe4acaa91
programistich <dzhos.oleksii@gmail.com>
2022-05-02 14:48:51 +0300

    refactor

--- components/keyscreen/impl/src/main/java/com/flipperdevices/keyscreen/impl/composable/actions/ComposableActionFlipper.kt
+++ components/keyscreen/impl/src/main/java/com/flipperdevices/keyscreen/impl/composable/actions/ComposableActionFlipper.kt
@@ -13,22 +13,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.flipperdevices.core.ui.R as DesignSystem
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
-import com.flipperdevices.keyscreen.impl.R
-import com.flipperdevices.core.ui.composable.painterResourceByKey
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun RowScope.ComposableActionFlipper(
@@ -39,14 +40,17 @@ fun RowScope.ComposableActionFlipper(
     onClick: () -> Unit,
 ) {
     val descriptionText = stringResource(descriptionId)
+    val modifier = Modifier
+        .clip(shape = RoundedCornerShape(30.dp))
+        .weight(1f)
+        .background(colorResource(id = DesignSystem.color.accent_secondary))
+        .clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = rememberRipple(),
+            onClick = onClick
+        )
 
-    Box(
-        modifier = Modifier
-            .clip(shape = RoundedCornerShape(30.dp))
-            .weight(1f)
-            .background(colorResource(id = DesignSystem.color.accent_secondary))
-            .clickable { onClick() }
-    ) {
+    Box(modifier = modifier) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -56,13 +60,13 @@ fun RowScope.ComposableActionFlipper(
         ) {
             Icon(
                 modifier = Modifier.size(size = 36.dp),
-                painter = painterResourceByKey(iconId),
+                painter = painterResource(iconId),
                 contentDescription = descriptionText,
                 tint = colorResource(tintId)
             )
             Text(
                 text = descriptionText,
-                fontWeight = FontWeight.Bold,
+                fontWeight = FontWeight.W500,
                 fontSize = 16.sp,
                 color = colorResource(descriptionColorId)
             )
