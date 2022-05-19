972ae6224c8207fb92cef9a460f02696b8f46b2f
LionZXY <nikita@kulikof.ru>
2022-05-19 10:48:58 +0400

    Small fixes with updater (#224)
    * Update folder now is file update name
    
    * Small fix with serial overflow
    
    * Check that file already exist
    
    * Add changelog line

--- components/updater/impl/src/main/java/com/flipperdevices/updater/impl/service/UploadFirmwareService.kt
+++ components/updater/impl/src/main/java/com/flipperdevices/updater/impl/service/UploadFirmwareService.kt
@@ -2,13 +2,19 @@ package com.flipperdevices.updater.impl.service
 
 import com.flipperdevices.bridge.api.manager.FlipperRequestApi
 import com.flipperdevices.bridge.api.model.FlipperRequest
+import com.flipperdevices.bridge.api.model.FlipperRequestPriority
+import com.flipperdevices.bridge.api.model.wrapToRequest
 import com.flipperdevices.bridge.protobuf.ProtobufConstants
 import com.flipperdevices.bridge.protobuf.streamToCommandFlow
+import com.flipperdevices.core.ktx.jre.md5
 import com.flipperdevices.core.log.LogTagProvider
 import com.flipperdevices.core.log.info
+import com.flipperdevices.protobuf.main
 import com.flipperdevices.protobuf.storage.file
+import com.flipperdevices.protobuf.storage.md5sumRequest
 import com.flipperdevices.protobuf.storage.writeRequest
 import java.io.File
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
 object UploadFirmwareService : LogTagProvider {
@@ -20,7 +26,12 @@ object UploadFirmwareService : LogTagProvider {
         pathOnFlipper: String,
         onProgressUpdate: (Long, Long) -> Unit
     ) {
-        val fileList = folder.walk().filterNot { it.isDirectory }.toList()
+        val fileList = folder.walk().filterNot { it.isDirectory }.toList().filterNot {
+            if (fileAlreadyUploaded(requestApi, it, File(pathOnFlipper, it.name).path)) {
+                info { "Skip $it because file already uploaded" }
+                return@filterNot true
+            } else return@filterNot false
+        }
         var totalBytesSend: Long = 0
         var totalSize: Long = 0
         fileList.forEach {
@@ -52,5 +63,27 @@ object UploadFirmwareService : LogTagProvider {
                 requestApi.request(requestFlow)
             }
         }
+    }
+
+    private suspend fun fileAlreadyUploaded(
+        requestApi: FlipperRequestApi,
+        file: File,
+        pathOnFlipper: String
+    ): Boolean {
+        val fileMd5 = file.inputStream().use {
+            it.md5()
+        }
+
+        val response = requestApi.request(
+            main {
+                storageMd5SumRequest = md5sumRequest {
+                    path = pathOnFlipper
+                }
+            }.wrapToRequest(FlipperRequestPriority.FOREGROUND)
+        ).first()
+        if (response.hasStorageMd5SumResponse()) {
+            return response.storageMd5SumResponse.md5Sum == fileMd5
+        }
+        return false
     }
 }
