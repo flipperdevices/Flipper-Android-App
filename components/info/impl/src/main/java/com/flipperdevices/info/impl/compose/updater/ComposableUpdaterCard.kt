4a58801c659e0987f6fc96332b227d1865207f9f
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2022-06-13 10:16:50 +0300

    Update error placeholder in firmware update (#237)
    **Background**
    
    **Changes**
    
    * Enum for error state
    * Move and edit resources 
    * Previews for error state
    
    **Test plan**
    
    * Do smth to get error state
    * Look to placeholder

--- components/info/impl/src/main/java/com/flipperdevices/info/impl/compose/updater/ComposableUpdaterCard.kt
+++ components/info/impl/src/main/java/com/flipperdevices/info/impl/compose/updater/ComposableUpdaterCard.kt
@@ -1,5 +1,7 @@
 package com.flipperdevices.info.impl.compose.updater
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -20,6 +22,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -32,11 +35,11 @@ import com.flipperdevices.info.impl.compose.info.ComposableInfoDivider
 import com.flipperdevices.info.impl.compose.info.ComposableUpdaterFirmwareVersionWithChoice
 import com.flipperdevices.info.impl.model.DeviceStatus
 import com.flipperdevices.info.impl.viewmodel.DeviceStatusViewModel
-import com.flipperdevices.updater.api.UpdateCardApi
 import com.flipperdevices.updater.api.UpdaterApi
 import com.flipperdevices.updater.api.UpdaterUIApi
 import com.flipperdevices.updater.model.FirmwareVersion
 import com.flipperdevices.updater.model.UpdateCardState
+import com.flipperdevices.updater.model.UpdateErrorType
 import com.flipperdevices.updater.model.UpdatingState
 
 @Composable
@@ -68,7 +71,10 @@ fun ComposableUpdaterCard(
 
         when (cardStateLocal) {
             is UpdateCardState.Error -> {
-                ComposableFirmwareUpdaterError(updateCardApi, cardStateLocal)
+                ComposableFirmwareUpdaterError(
+                    typeError = cardStateLocal.type,
+                    retryUpdate = updateCardApi::retry
+                )
             }
             UpdateCardState.InProgress -> ComposableFirmwareUpdaterInProgress()
             is UpdateCardState.NoUpdate -> ComposableFirmwareUpdaterContent(
@@ -85,18 +91,48 @@ fun ComposableUpdaterCard(
     }
 }
 
+@StringRes
+fun getTitleByUpdateError(type: UpdateErrorType): Int {
+    return when (type) {
+        UpdateErrorType.NO_INTERNET -> R.string.update_card_error_no_internet_title
+        UpdateErrorType.UNABLE_TO_SERVER -> R.string.update_card_error_server_request_title
+        UpdateErrorType.NO_SD_CARD -> R.string.update_card_error_no_sd_title
+    }
+}
+
+@StringRes
+fun getDescriptionByUpdateError(type: UpdateErrorType): Int {
+    return when (type) {
+        UpdateErrorType.NO_INTERNET -> R.string.update_card_error_no_internet_desc
+        UpdateErrorType.UNABLE_TO_SERVER -> R.string.update_card_error_server_request_desc
+        UpdateErrorType.NO_SD_CARD -> R.string.update_card_error_no_sd_desc
+    }
+}
+
+@DrawableRes
+fun getImageByUpdateError(type: UpdateErrorType): Int {
+    return when (type) {
+        UpdateErrorType.NO_INTERNET -> R.drawable.ic_no_internet
+        UpdateErrorType.UNABLE_TO_SERVER -> R.drawable.ic_server_error
+        UpdateErrorType.NO_SD_CARD -> R.drawable.ic_no_sd
+    }
+}
+
 @Composable
 private fun ComposableFirmwareUpdaterError(
-    updateCardApi: UpdateCardApi,
-    error: UpdateCardState.Error
+    typeError: UpdateErrorType,
+    retryUpdate: () -> Unit = {}
 ) {
-    val title = stringResource(error.titleId)
+    val title = stringResource(id = getTitleByUpdateError(typeError))
+    val description = stringResource(id = getDescriptionByUpdateError(typeError))
+    val image = painterResourceByKey(id = getImageByUpdateError(typeError))
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResourceByKey(error.iconId),
+            painter = image,
             contentDescription = title
         )
         Text(
@@ -109,7 +145,7 @@ private fun ComposableFirmwareUpdaterError(
         )
         Text(
             modifier = Modifier.padding(top = 2.dp, start = 12.dp, end = 12.dp),
-            text = stringResource(error.descriptionId),
+            text = description,
             fontWeight = FontWeight.W400,
             fontSize = 14.sp,
             color = colorResource(DesignSystem.color.black_30),
@@ -121,7 +157,7 @@ private fun ComposableFirmwareUpdaterError(
         modifier = Modifier
             .clickable(
                 indication = rememberRipple(),
-                onClick = updateCardApi::retry,
+                onClick = retryUpdate,
                 interactionSource = remember { MutableInteractionSource() }
             )
             .fillMaxWidth()
@@ -172,4 +208,17 @@ private fun ComposableFirmwareUpdaterContent(
     }
     ComposableInfoDivider()
     ComposableUpdateButton(updaterUiApi, updateCardState)
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+private fun ComposableFirmwareUpdaterErrorPreview() {
+    Column {
+        ComposableFirmwareUpdaterError(UpdateErrorType.NO_INTERNET)
+        ComposableFirmwareUpdaterError(UpdateErrorType.UNABLE_TO_SERVER)
+        ComposableFirmwareUpdaterError(UpdateErrorType.NO_SD_CARD)
+    }
 }
