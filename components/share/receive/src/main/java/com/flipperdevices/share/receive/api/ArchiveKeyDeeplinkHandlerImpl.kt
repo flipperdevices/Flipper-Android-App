7545750b1a39668ebf33be188325fc4de36b8729
LionZXY <nikita@kulikof.ru>
2022-06-15 20:35:59 +0400

    Add open_save_key

--- components/share/receive/src/main/java/com/flipperdevices/share/receive/api/ArchiveKeyDeeplinkHandlerImpl.kt
+++ components/share/receive/src/main/java/com/flipperdevices/share/receive/api/ArchiveKeyDeeplinkHandlerImpl.kt
@@ -4,6 +4,8 @@ import com.flipperdevices.core.di.AppGraph
 import com.flipperdevices.deeplink.api.DispatcherPriority
 import com.flipperdevices.deeplink.model.Deeplink
 import com.flipperdevices.deeplink.model.DeeplinkContent
+import com.flipperdevices.metric.api.MetricApi
+import com.flipperdevices.metric.api.events.SimpleEvent
 import com.flipperdevices.share.api.ArchiveKeyDeeplinkHandler
 import com.flipperdevices.share.receive.fragments.KeyReceiveFragment
 import com.github.terrakok.cicerone.Router
@@ -12,7 +14,9 @@ import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
 
 @ContributesBinding(AppGraph::class)
-class ArchiveKeyDeeplinkHandlerImpl @Inject constructor() : ArchiveKeyDeeplinkHandler {
+class ArchiveKeyDeeplinkHandlerImpl @Inject constructor(
+    private val metricApi: MetricApi
+) : ArchiveKeyDeeplinkHandler {
     override fun isSupportLink(link: Deeplink): DispatcherPriority? {
         if (link.path == null || link.content == null) {
             return null
@@ -25,6 +29,7 @@ class ArchiveKeyDeeplinkHandlerImpl @Inject constructor() : ArchiveKeyDeeplinkHa
 
     override fun processLink(router: Router, link: Deeplink) {
         val fragmentScreen = FragmentScreen { KeyReceiveFragment.newInstance(link) }
+        metricApi.reportSimpleEvent(SimpleEvent.OPEN_SAVE_KEY)
         router.navigateTo(fragmentScreen)
     }
 }
