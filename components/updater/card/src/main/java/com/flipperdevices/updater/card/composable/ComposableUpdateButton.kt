cf0e82a9d8245e68f737a3ea2df3b3b2be8903bc
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-06-17 18:35:56 +0300

    Placeholder in firmware button install/update

--- components/updater/card/src/main/java/com/flipperdevices/updater/card/composable/ComposableUpdateButton.kt
+++ components/updater/card/src/main/java/com/flipperdevices/updater/card/composable/ComposableUpdateButton.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -26,23 +28,34 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.flipperdevices.core.ui.res.R as DesignSystem
+import com.flipperdevices.core.ui.ktx.placeholderConnecting
 import com.flipperdevices.updater.card.R
 import com.flipperdevices.updater.card.composable.dialogs.ComposableUpdateRequest
-import com.flipperdevices.updater.fonts.R as Fonts
+import com.flipperdevices.updater.model.DistributionFile
+import com.flipperdevices.updater.model.FirmwareChannel
+import com.flipperdevices.updater.model.FirmwareVersion
 import com.flipperdevices.updater.model.UpdateCardState
+import com.flipperdevices.core.ui.res.R as DesignSystem
+import com.flipperdevices.updater.fonts.R as Fonts
 
 @Composable
 fun ComposableUpdateButton(
-    updateCardState: UpdateCardState
+    updateCardState: UpdateCardState,
+    inProgress: Boolean
 ) {
     var buttonModifier = Modifier.padding(all = 12.dp)
     var pendingUpdateRequest by remember {
         mutableStateOf<UpdateCardState.UpdateAvailable?>(null)
     }
     val localUpdaterRequest = pendingUpdateRequest
+
+    if (inProgress) {
+        ComposableUpdateButtonPlaceholder(buttonModifier)
+        return
+    }
 
     if (localUpdaterRequest != null) {
         ComposableUpdateRequest(pendingUpdateRequest = localUpdaterRequest) {
@@ -86,6 +99,16 @@ fun ComposableUpdateButton(
 }
 
 @Composable
+fun ComposableUpdateButtonPlaceholder(buttonModifier: Modifier) {
+    Box(
+        modifier = buttonModifier
+            .height(46.dp)
+            .fillMaxWidth()
+            .placeholderConnecting(shape = 9)
+    )
+}
+
+@Composable
 private fun ComposableUpdateButtonContent(
     buttonModifier: Modifier,
     @StringRes textId: Int,
@@ -123,5 +146,37 @@ private fun ComposableUpdateButtonContent(
             fontSize = 12.sp,
             color = colorResource(DesignSystem.color.black_16)
         )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun ComposableUpdateButtonPreview() {
+    val version = FirmwareVersion(
+        channel = FirmwareChannel.RELEASE,
+        version = "1.1.1"
+    )
+    val updateCardState = setOf(
+        UpdateCardState.NoUpdate(flipperVersion = version),
+        UpdateCardState.UpdateAvailable(
+            fromVersion = version,
+            lastVersion = version,
+            updaterDist = DistributionFile(
+                url = "123",
+                sha256 = "123"
+            ),
+            isOtherChannel = false
+        )
+    )
+    Column(
+        modifier = Modifier.fillMaxSize().padding(12.dp).background(colorResource(id = DesignSystem.color.background))
+    ) {
+        updateCardState.forEach {
+            ComposableUpdateButton(it, false)
+            ComposableUpdateButton(it, true)
+        }
     }
 }
