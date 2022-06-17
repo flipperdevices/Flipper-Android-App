e97cdf31e03082ffa54b0d9fde9f4d2dfc75a7ef
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-06-17 16:58:27 +0300

    Placeholder in device information

--- components/info/impl/src/main/java/com/flipperdevices/info/impl/compose/info/ComposableFirmwareVersion.kt
+++ components/info/impl/src/main/java/com/flipperdevices/info/impl/compose/info/ComposableFirmwareVersion.kt
@@ -1,13 +1,22 @@
 package com.flipperdevices.info.impl.compose.info
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.flipperdevices.info.impl.R
 import com.flipperdevices.info.shared.ComposableDeviceInfoRow
 import com.flipperdevices.info.shared.ComposableDeviceInfoRowText
 import com.flipperdevices.info.shared.ComposableDeviceInfoRowWithText
 import com.flipperdevices.info.shared.getColorByChannel
 import com.flipperdevices.info.shared.getTextByVersion
+import com.flipperdevices.updater.model.FirmwareChannel
 import com.flipperdevices.updater.model.FirmwareVersion
+import com.flipperdevices.core.ui.res.R as DesignSystem
 
 @Composable
 fun ComposableFirmwareVersion(
@@ -53,4 +62,27 @@ fun ComposableFirmwareBuildDate(
         firmwareVersionInProgress,
         firmwareVersion.buildDate
     )
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+private fun ComposableFirmwareVersionAndBuildDate() {
+    val firmwareVersion = FirmwareVersion(
+        channel = FirmwareChannel.DEV,
+        version = "1.1.1",
+        buildDate = "today"
+    )
+    Column(Modifier.width(300.dp).background(colorResource(id = DesignSystem.color.background))) {
+        ComposableFirmwareVersion(firmwareVersion, true)
+        ComposableFirmwareBuildDate(firmwareVersion, true)
+
+        ComposableFirmwareVersion(null, true)
+        ComposableFirmwareBuildDate(null, true)
+
+        ComposableFirmwareVersion(null, false)
+        ComposableFirmwareBuildDate(null, false)
+    }
 }
