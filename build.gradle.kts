d60044649050e3b4c2e8a5ac8432a55bdc9d5101
Andrey Stepankov <rougsig@gmail.com>
2022-06-20 14:27:20 +0300

    Add dumpApkVersion gradle task (#251)
    **Background**
    Preparation for https://github.com/flipperdevices/Flipper-Android-App/issues/46
    Every build generate unique version_code.
    We can't easily extract version_code from apk or source code.
    version_code and version_name should be provided for f-droid bot.
    
    **Changes**
    Added gradle task, modified github ci workflows.
    
    **Test plan**
    Green internal and release pipeline.
    New github release contains `apk-version.properties` or `apk-version-release.properties` file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,3 +37,25 @@ subprojects {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
 }
+
+tasks.register("dumpApkVersion") {
+    val CODE_PROP = "version_code"
+    val NAME_PROP = "version_name"
+
+    val outputFileProvider = layout.buildDirectory.file("version/apk-version.properties")
+
+    inputs.property(CODE_PROP, provider { ApkConfig.VERSION_CODE })
+    inputs.property(NAME_PROP, provider { ApkConfig.VERSION_NAME })
+    outputs.file(outputFileProvider)
+
+    doLast {
+        val outputFile = outputFileProvider.get().asFile
+        outputFile.deleteOnExit()
+        outputFile.parentFile.mkdirs()
+
+        val props = java.util.Properties()
+        props.setProperty(CODE_PROP, inputs.properties[CODE_PROP].toString())
+        props.setProperty(NAME_PROP, inputs.properties[NAME_PROP].toString())
+        props.store(outputFile.writer(), /* comments = */ null)
+    }
+}
