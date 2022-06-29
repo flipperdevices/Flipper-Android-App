49936271882fa2a1b143dbd69f7b04538b91dc34
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-06-29 09:07:27 +0300

    move fonts to res module

--- components/core/ui/theme/src/main/kotlin/com/flipperdevices/core/ui/theme/FlipperTypography.kt
+++ components/core/ui/theme/src/main/kotlin/com/flipperdevices/core/ui/theme/FlipperTypography.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import com.flipperdevices.core.ui.res.R
 
 data class FlipperTypography(
     val titleB20: TextStyle,
