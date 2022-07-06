500a2578e7a69393b554b6ae8ac2911d25aa7998
LionZXY <nikita@kulikof.ru>
2022-07-06 06:23:59 +0400

    Add black flipper on update screen

--- components/updater/screen/src/main/java/com/flipperdevices/updater/screen/di/UpdaterComponent.kt
+++ components/updater/screen/src/main/java/com/flipperdevices/updater/screen/di/UpdaterComponent.kt
@@ -2,11 +2,13 @@ package com.flipperdevices.updater.screen.di
 
 import com.flipperdevices.core.di.AppGraph
 import com.flipperdevices.updater.screen.fragments.UpdaterFragment
+import com.flipperdevices.updater.screen.viewmodel.FlipperColorViewModel
 import com.flipperdevices.updater.screen.viewmodel.UpdaterViewModel
 import com.squareup.anvil.annotations.ContributesTo
 
 @ContributesTo(AppGraph::class)
 interface UpdaterComponent {
     fun inject(viewModel: UpdaterViewModel)
+    fun inject(viewModel: FlipperColorViewModel)
     fun inject(fragment: UpdaterFragment)
 }
---
+++ components/updater/screen/src/main/java/com/flipperdevices/updater/screen/viewmodel/FlipperColorViewModel.kt
@@ -0,0 +1,30 @@
+package com.flipperdevices.updater.screen.viewmodel
+
+import androidx.datastore.core.DataStore
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.flipperdevices.core.di.ComponentHolder
+import com.flipperdevices.core.preference.pb.HardwareColor
+import com.flipperdevices.core.preference.pb.PairSettings
+import com.flipperdevices.updater.screen.di.UpdaterComponent
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class FlipperColorViewModel : ViewModel() {
+    private val colorFlipperState = MutableStateFlow(HardwareColor.WHITE)
+
+    @Inject
+    lateinit var settings: DataStore<PairSettings>
+
+    init {
+        ComponentHolder.component<UpdaterComponent>().inject(this)
+        settings.data.onEach {
+            colorFlipperState.emit(it.hardwareColor)
+        }.launchIn(viewModelScope)
+    }
+
+    fun getFlipperColor(): StateFlow<HardwareColor> = colorFlipperState
+}
