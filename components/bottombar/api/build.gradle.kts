b7b3be83b0fcc6f7e81646401b6182bf88cc4c48
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-07-13 10:33:26 +0300

    test for equals TabState object and hashcode

--- components/bottombar/api/build.gradle.kts
+++ components/bottombar/api/build.gradle.kts
@@ -6,4 +6,9 @@ dependencies {
     implementation(libs.cicerone)
 
     implementation(libs.compose.ui)
+
+    // Testing
+    testImplementation(projects.components.core.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito.kotlin)
 }
