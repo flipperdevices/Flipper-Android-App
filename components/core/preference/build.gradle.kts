79483f33ed00fff7b7082bbc77ce6b5b6fcda91c
LionZXY <nikita@kulikof.ru>
2022-07-15 22:33:25 +0400

    Nope

--- components/core/preference/build.gradle.kts
+++ components/core/preference/build.gradle.kts
@@ -23,16 +23,6 @@ dependencies {
 
 // Kotlin 1.7.0 breaks kapt processing for protobuf generated java sources
 // https://youtrack.jetbrains.com/issue/KT-52761/Kotlin-170-breaks-kapt-processing-for-protobuf-generated-java-sources
-android.buildTypes.configureEach {
-    val buildType = this
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KaptGenerateStubs>().configureEach {
-        kotlinOptions {
-            freeCompilerArgs = listOf(
-                "-Xjava-source-roots=$projectDir/build/generated/source/proto/${buildType.name}/java"
-            )
-        }
-    }
-}
 
 protobuf {
     protoc {
