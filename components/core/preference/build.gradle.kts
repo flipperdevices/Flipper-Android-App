17a241f0431861ed9baa6f9d9f2562d7e591db6f
rougsig <rougsig@gmail.com>
2022-07-16 17:04:31 +0300

    Add fix for gradle-protobuf-plugin and kapt

--- components/core/preference/build.gradle.kts
+++ components/core/preference/build.gradle.kts
@@ -23,6 +23,12 @@ dependencies {
 
 // Kotlin 1.7.0 breaks kapt processing for protobuf generated java sources
 // https://youtrack.jetbrains.com/issue/KT-52761/Kotlin-170-breaks-kapt-processing-for-protobuf-generated-java-sources
+android.libraryVariants.all {
+    android.sourceSets[name].apply {
+        java.srcDir("build/generated/source/proto/$name/java")
+        kotlin.srcDir("build/generated/source/proto/$name/kotlin")
+    }
+}
 
 protobuf {
     protoc {
