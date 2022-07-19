6e7811cd6f00c03ff3db01f47d6a101a07f7138b
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-07-19 19:09:02 +0300

    remove preview

--- components/updater/screen/src/main/java/com/flipperdevices/updater/screen/composable/ComposableUpdaterScreen.kt
+++ components/updater/screen/src/main/java/com/flipperdevices/updater/screen/composable/ComposableUpdaterScreen.kt
@@ -18,17 +18,12 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.flipperdevices.core.preference.pb.HardwareColor
 import com.flipperdevices.core.ui.res.R as DesignSystem
-import com.flipperdevices.core.ui.theme.FlipperThemeInternal
 import com.flipperdevices.core.ui.theme.LocalPallet
 import com.flipperdevices.core.ui.theme.LocalTypography
-import com.flipperdevices.updater.model.FirmwareChannel
-import com.flipperdevices.updater.model.FirmwareVersion
 import com.flipperdevices.updater.screen.R
-import com.flipperdevices.updater.screen.model.FailedReason
 import com.flipperdevices.updater.screen.model.UpdaterScreenState
 
 @Composable
@@ -116,166 +111,6 @@ private fun CancelButton(
             textAlign = TextAlign.Center,
             color = LocalPallet.current.accentSecond,
             style = LocalTypography.current.buttonM16
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenNotStartedPreview() {
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.NotStarted,
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenCancelingSynchronizationPreview() {
-    val version = FirmwareVersion(
-        channel = FirmwareChannel.RELEASE,
-        version = "0.65.2"
-    )
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.CancelingSynchronization(version = version),
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenDownloadingFromNetworkPreview() {
-    val version = FirmwareVersion(
-        channel = FirmwareChannel.RELEASE,
-        version = "0.65.2"
-    )
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.DownloadingFromNetwork(
-                version = version,
-                percent = 0.5f
-            ),
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenUploadOnFlipperPreview() {
-    val version = FirmwareVersion(
-        channel = FirmwareChannel.RELEASE,
-        version = "0.65.2"
-    )
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState =
-            UpdaterScreenState.UploadOnFlipper(version = version, percent = 0.5f),
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenFailedNetworkPreview() {
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.Failed(FailedReason.DOWNLOAD_FROM_NETWORK),
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenFailedOnFlipperPreview() {
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.Failed(FailedReason.UPLOAD_ON_FLIPPER),
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenRebootingPreview() {
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.Rebooting,
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenCancelingUpdatePreview() {
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.CancelingUpdate,
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
-        )
-    }
-}
-
-@Composable
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-private fun ComposableUpdaterScreenFinishPreview() {
-    FlipperThemeInternal {
-        ComposableUpdaterScreen(
-            updaterScreenState = UpdaterScreenState.Finish,
-            flipperColor = HardwareColor.BLACK,
-            onCancel = {},
-            onRetry = {}
         )
     }
 }
