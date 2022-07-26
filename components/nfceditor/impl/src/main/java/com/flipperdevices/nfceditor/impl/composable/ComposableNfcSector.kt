11466c7d8ead26d2b6fbd939b5a7e5fa2d431a11
LionZXY <nikita@kulikof.ru>
2022-07-26 19:00:11 +0400

    Add sector title

--- components/nfceditor/impl/src/main/java/com/flipperdevices/nfceditor/impl/composable/ComposableNfcSector.kt
+++ components/nfceditor/impl/src/main/java/com/flipperdevices/nfceditor/impl/composable/ComposableNfcSector.kt
@@ -1,11 +1,21 @@
 package com.flipperdevices.nfceditor.impl.composable
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.flipperdevices.core.ui.theme.LocalPallet
+import com.flipperdevices.core.ui.theme.LocalTypography
+import com.flipperdevices.nfceditor.impl.R
 import com.flipperdevices.nfceditor.impl.model.NfcEditorState
 import com.flipperdevices.nfceditor.impl.viewmodel.NfcEditorViewModel
 
 @Composable
+@Suppress("MagicNumber")
 fun ComposableNfcSector(
     nfcEditorViewModel: NfcEditorViewModel,
     nfcEditorState: NfcEditorState,
@@ -14,6 +24,17 @@ fun ComposableNfcSector(
     scaleFactor: Float
 ) {
     Column {
+        Text(
+            modifier = Modifier.padding(
+                top = (scaleFactor * 24).dp,
+                start = (scaleFactor * WIDTH_LINE_INDEX_DP * maxIndexSymbolCount).dp
+            ),
+            text = stringResource(R.string.nfceditor_sector_title, sectorIndex),
+            color = LocalPallet.current.text100,
+            fontSize = (scaleFactor * 12).sp,
+            style = LocalTypography.current.subtitleB12
+        )
+
         nfcEditorState.sectors[sectorIndex].lines.forEachIndexed { lineIndex, line ->
             ComposableNfcLine(
                 sectorIndex = sectorIndex,
