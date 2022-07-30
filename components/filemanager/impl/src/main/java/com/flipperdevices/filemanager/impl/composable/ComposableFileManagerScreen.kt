c3c546548767e166c4b8cde9d949db9e3fdefa3a
LionZXY <nikita@kulikof.ru>
2022-07-31 01:06:33 +0400

    Add ComposableEllipsizeStartText

--- components/filemanager/impl/src/main/java/com/flipperdevices/filemanager/impl/composable/ComposableFileManagerScreen.kt
+++ components/filemanager/impl/src/main/java/com/flipperdevices/filemanager/impl/composable/ComposableFileManagerScreen.kt
@@ -6,23 +6,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import com.flipperdevices.deeplink.api.DeepLinkParser
 import com.flipperdevices.deeplink.model.DeeplinkContent
 import com.flipperdevices.filemanager.impl.R
+import com.flipperdevices.filemanager.impl.composable.bar.ComposableEllipsizeStartText
 import com.flipperdevices.filemanager.impl.composable.list.ComposableFileManagerContent
 import com.flipperdevices.filemanager.impl.model.FileItem
 import com.flipperdevices.filemanager.impl.model.FileManagerState
 import kotlinx.coroutines.runBlocking
-
-private const val MAX_PATH_LENGTH = 16
 
 @Composable
 fun ComposableFileManagerScreen(
@@ -65,13 +62,8 @@ fun ComposableFileManagerScreen(
 private fun ComposableFileManagerTopBar(path: String, onClickSaveButton: () -> Unit) {
     TopAppBar(
         title = {
-            val textWithLimitation = if (path.length > MAX_PATH_LENGTH) {
-                "...${path.takeLast(MAX_PATH_LENGTH)}"
-            } else path
-            Text(
-                text = textWithLimitation,
-                maxLines = 1,
-                overflow = TextOverflow.Clip
+            ComposableEllipsizeStartText(
+                text = path
             )
         },
         actions = {
@@ -89,4 +81,4 @@ private fun ComposableFileManagerTopBar(path: String, onClickSaveButton: () -> U
     )
 }
 
-private fun isAbleToSafe(path: String) = path.startsWith("/ext/")
+private fun isAbleToSafe(path: String) = path.startsWith("/ext")
