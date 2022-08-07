dc715279ca6542b8eddcf56fe2ffdf0a187bcf96
LionZXY <nikita@kulikof.ru>
2022-08-07 18:40:56 +0300

    Update synchronization status instantly and respect the flag (#322)
    **Background**
    
    For now we don't start synchronization instantly and also not respect `force` flag
    
    **Changes**
    
    - Update synchronization state in UI thread first time
    - Mark dirty only if synchronization is force
    
    **Test plan**
    
    Try start synchronization two or more time via pull to refresh

--- components/bridge/synchronization/api/src/main/java/com/flipperdevices/bridge/synchronization/api/SynchronizationApi.kt
+++ components/bridge/synchronization/api/src/main/java/com/flipperdevices/bridge/synchronization/api/SynchronizationApi.kt
@@ -3,6 +3,10 @@ package com.flipperdevices.bridge.synchronization.api
 import kotlinx.coroutines.flow.StateFlow
 
 interface SynchronizationApi {
+    /**
+     * @param force if true marks the synchronization dirty
+     * and starts the second synchronization after the first synchronization is finished.
+     */
     fun startSynchronization(force: Boolean = false)
 
     fun getSynchronizationState(): StateFlow<SynchronizationState>
