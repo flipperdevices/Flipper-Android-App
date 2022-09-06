ce06e0906e5d766f7a3fb1de8387bb57bc25fa8f
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-09-06 18:39:27 +0300

    Remove buildSrc

--- buildSrc/build/kotlin-dsl/plugins-blocks/extracted/androidApplication.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("io.sentry.android.gradle")
-}
--- buildSrc/build/kotlin-dsl/plugins-blocks/extracted/androidLibrary.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    kotlin("kapt")
-}
