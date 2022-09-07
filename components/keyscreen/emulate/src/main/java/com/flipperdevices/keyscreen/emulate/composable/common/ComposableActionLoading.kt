84dc1e024b3b334c2d2611500f9abfc9e52c448b
LionZXY <nikita@kulikof.ru>
2022-09-07 10:27:15 +0300

    Fix placeholder

--- components/keyscreen/emulate/src/main/java/com/flipperdevices/keyscreen/emulate/composable/common/ComposableActionLoading.kt
+++ components/keyscreen/emulate/src/main/java/com/flipperdevices/keyscreen/emulate/composable/common/ComposableActionLoading.kt
@@ -1,9 +1,11 @@
 package com.flipperdevices.keyscreen.emulate.composable.common
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.flipperdevices.core.ui.theme.FlipperThemeInternal
 import com.flipperdevices.core.ui.theme.LocalPallet
 import com.flipperdevices.keyscreen.emulate.R
@@ -20,12 +22,13 @@ fun ComposableActionLoading(
     val placeholderColor = LocalPallet.current.text8.copy(alpha = 0.2f)
     ComposableEmulateButtonWithText(
         modifier = modifier,
-        buttonModifier = modifier.placeholder(
+        buttonModifier = Modifier.placeholder(
             visible = true,
             color = placeholderColor,
             highlight = PlaceholderHighlight.shimmer(
                 highlightColor = LocalPallet.current.placeholder
-            )
+            ),
+            shape = RoundedCornerShape(16.dp)
         ),
         buttonTextId = R.string.keyscreen_loading,
         color = LocalPallet.current.text8,
