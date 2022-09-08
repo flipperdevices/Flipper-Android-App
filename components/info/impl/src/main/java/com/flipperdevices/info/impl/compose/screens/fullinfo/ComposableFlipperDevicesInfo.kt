98f1d2924263166d8f335d3ef2b2aebd2dd44f69
LionZXY <nikita@kulikof.ru>
2022-09-08 11:12:12 +0300

    Remove easter egg

--- components/info/impl/src/main/java/com/flipperdevices/info/impl/compose/screens/fullinfo/ComposableFlipperDevicesInfo.kt
+++ components/info/impl/src/main/java/com/flipperdevices/info/impl/compose/screens/fullinfo/ComposableFlipperDevicesInfo.kt
@@ -1,15 +1,8 @@
 package com.flipperdevices.info.impl.compose.screens.fullinfo
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.flipperdevices.bridge.api.model.FlipperDeviceInfo
 import com.flipperdevices.info.impl.R
 import com.flipperdevices.info.shared.ComposableDeviceInfoRow
@@ -76,18 +69,6 @@ private fun ComposableCountryRow(
     } else {
         ComposableDeviceInfoRow(titleId, inProgress) { modifier ->
             Row(verticalAlignment = Alignment.CenterVertically) {
-                if (value == "RU") {
-                    Image(
-                        modifier = Modifier.height(height = 16.dp)
-                            .padding(horizontal = 6.dp),
-                        painter = painterResource(
-                            com.flipperdevices.core.ui.res.R.drawable.rus_new_flag
-                        ),
-                        contentDescription = stringResource(
-                            R.string.full_info_hardware_region_provisioned_ru
-                        )
-                    )
-                }
                 ComposableDeviceInfoRowText(modifier = modifier, text = value)
             }
         }
