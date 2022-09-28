ca409eb09262850a8cfd65c3d32bcfb21a5743e9
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-09-28 19:36:31 +0300

    fix test

--- components/updater/card/src/test/kotlin/com/flipperdevices/updater/card/viewmodel/UpdateRequestViewModelTest.kt
+++ components/updater/card/src/test/kotlin/com/flipperdevices/updater/card/viewmodel/UpdateRequestViewModelTest.kt
@@ -1,20 +1,29 @@
 package com.flipperdevices.updater.card.viewmodel
 
+import android.content.Context
+import android.net.Uri
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.flipperdevices.bridge.api.model.FlipperGATTInformation
 import com.flipperdevices.bridge.service.api.FlipperServiceApi
 import com.flipperdevices.bridge.service.api.provider.FlipperServiceProvider
+import com.flipperdevices.core.ktx.jre.getClearName
+import com.flipperdevices.deeplink.api.DeepLinkParser
+import com.flipperdevices.deeplink.model.Deeplink
+import com.flipperdevices.deeplink.model.DeeplinkContent
 import com.flipperdevices.updater.api.UpdaterUIApi
 import com.flipperdevices.updater.card.model.BatteryState
+import com.flipperdevices.updater.card.model.UpdatePending
 import com.flipperdevices.updater.model.DistributionFile
 import com.flipperdevices.updater.model.FirmwareChannel
 import com.flipperdevices.updater.model.FirmwareVersion
+import com.flipperdevices.updater.model.InternalStorageFirmware
 import com.flipperdevices.updater.model.OfficialFirmware
-import com.flipperdevices.updater.model.UpdateCardState
 import com.flipperdevices.updater.model.UpdateRequest
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import java.io.File
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
@@ -30,10 +39,11 @@ class UpdateRequestViewModelTest {
     private val serviceProvider: FlipperServiceProvider = mockk(relaxUnitFun = true)
     private val serviceApi: FlipperServiceApi = mockk()
     private val updaterUIApi: UpdaterUIApi = mockk(relaxUnitFun = true)
+    private val deeplinkParser: DeepLinkParser = mockk()
     private val viewModel = UpdateRequestViewModel(
         serviceProvider = serviceProvider,
         updaterUIApi = updaterUIApi,
-        deeplinkParser = mockk()
+        deeplinkParser = deeplinkParser
     )
 
     @Test
@@ -73,18 +83,17 @@ class UpdateRequestViewModelTest {
     }
 
     @Test
-    fun `verify call open updater screen`() {
+    fun `verify call open update from official source`() {
         val fmVersion = FirmwareVersion(channel = FirmwareChannel.DEV, version = "")
-        val file = DistributionFile(url = "", sha256 = "")
+        val content = OfficialFirmware(DistributionFile(url = "", sha256 = ""))
         viewModel.openUpdate(
-            UpdateCardState.UpdateAvailable(
-                update = UpdateRequest(
+            UpdatePending.Request(
+                updateRequest = UpdateRequest(
                     updateFrom = fmVersion,
                     updateTo = fmVersion,
-                    content = OfficialFirmware(DistributionFile(url = "", sha256 = "")),
+                    content = content,
                     changelog = null
-                ),
-                isOtherChannel = true
+                )
             )
         )
         verify {
@@ -96,7 +105,45 @@ class UpdateRequestViewModelTest {
                     Assert.assertEquals(it.updateFrom, fmVersion)
                     Assert.assertEquals(it.updateTo, fmVersion)
                     Assert.assertEquals(it.changelog, null)
-                    Assert.assertEquals(it.content, file)
+                    Assert.assertEquals(it.content, content)
+                }
+            )
+        }
+    }
+
+    @Test
+    fun `verify call open update from internal storage`() {
+        val currentFW = FirmwareVersion(channel = FirmwareChannel.DEV, version = "")
+        val context: Context = mockk()
+        val path = "/test/text.txt"
+        val file = File(path)
+        val uri = Uri.EMPTY
+        coEvery { deeplinkParser.fromUri(context, uri) } coAnswers {
+            Deeplink(path = null, content = DeeplinkContent.InternalStorageFile(path))
+        }
+        viewModel.openUpdate(
+            UpdatePending.URI(
+                uri = uri,
+                context = context,
+                currentVersion = currentFW
+            )
+        )
+        verify {
+            updaterUIApi.openUpdateScreen(
+                withArg {
+                    Assert.assertFalse(it)
+                },
+                withArg {
+                    Assert.assertEquals(it.updateFrom, currentFW)
+                    Assert.assertEquals(
+                        it.updateTo,
+                        FirmwareVersion(
+                            channel = FirmwareChannel.CUSTOM,
+                            version = file.getClearName()
+                        )
+                    )
+                    Assert.assertEquals(it.changelog, null)
+                    assert(it.content is InternalStorageFirmware)
                 }
             )
         }
