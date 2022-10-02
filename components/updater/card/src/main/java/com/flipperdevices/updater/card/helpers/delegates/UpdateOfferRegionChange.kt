a06fd33080c9c38f5c9b4526393b1c7204e5aa50
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-10-02 13:37:14 +0300

    Get last region from flipper

--- components/updater/card/src/main/java/com/flipperdevices/updater/card/helpers/delegates/UpdateOfferRegionChange.kt
+++ components/updater/card/src/main/java/com/flipperdevices/updater/card/helpers/delegates/UpdateOfferRegionChange.kt
@@ -1,35 +1,46 @@
 package com.flipperdevices.updater.card.helpers.delegates
 
-import androidx.datastore.core.DataStore
+import com.flipperdevices.bridge.api.model.FlipperRequestPriority
+import com.flipperdevices.bridge.api.model.wrapToRequest
+import com.flipperdevices.bridge.api.utils.Constants
 import com.flipperdevices.bridge.service.api.FlipperServiceApi
 import com.flipperdevices.core.di.AppGraph
-import com.flipperdevices.core.preference.pb.Settings
+import com.flipperdevices.protobuf.Flipper
+import com.flipperdevices.protobuf.Flipper.Region
+import com.flipperdevices.protobuf.main
+import com.flipperdevices.protobuf.storage.readRequest
 import com.flipperdevices.updater.subghz.helpers.SubGhzProvisioningHelper
 import com.squareup.anvil.annotations.ContributesMultibinding
+import java.nio.charset.Charset
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 @ContributesMultibinding(scope = AppGraph::class, boundType = UpdateOfferDelegate::class)
 class UpdateOfferRegionChange @Inject constructor(
-    private val dataStoreSettings: DataStore<Settings>,
     private val subGhzProvisioningHelper: SubGhzProvisioningHelper
 ) : UpdateOfferDelegate {
 
-    private suspend fun isRegionChanged(lastProvidedRegion: String): Boolean {
-        return try {
-            // In this case, when the region is received, we can get an error.
-            // When we receive new firmware we handle errors with the internet(and setup state),
-            // so in this case we will return that the region has changed
-            // to be sure to flash a new region if we got into a bad timing for obtaining a region
-            val currentRegion = subGhzProvisioningHelper.getRegion()
-            currentRegion != lastProvidedRegion
-        } catch (@Suppress("SwallowedException") exception: Exception) {
-            true
-        }
-    }
-
     override fun isRequire(serviceApi: FlipperServiceApi): Flow<Boolean> {
-        return dataStoreSettings.data.map { isRegionChanged(it.lastProvidedRegion) }
+        return serviceApi.requestApi.request(
+            main {
+                storageReadRequest = readRequest {
+                    path = Constants.PATH.REGION_FILE
+                }
+            }.wrapToRequest(FlipperRequestPriority.BACKGROUND)
+        ).map { response ->
+            if (response.commandStatus != Flipper.CommandStatus.OK) return@map true
+            if (!response.hasStorageReadResponse()) return@map true
+
+            try {
+                val array = response.storageReadResponse.file.data
+                val region = Region.parseFrom(array)
+                val code = region.countryCode.toString(Charset.forName("ASCII"))
+
+                return@map subGhzProvisioningHelper.getRegion() != code
+            } catch (@Suppress("SwallowedException") exception: Exception) {
+                return@map true
+            }
+        }
     }
 }
