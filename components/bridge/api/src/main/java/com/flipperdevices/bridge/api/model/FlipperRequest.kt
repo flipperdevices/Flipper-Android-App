d2c1b3d362d452dd5475b11de6ef14fae28ff9b0
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-10-04 11:16:39 +0300

    remove equals reserve proto

--- components/bridge/api/src/main/java/com/flipperdevices/bridge/api/model/FlipperRequest.kt
+++ components/bridge/api/src/main/java/com/flipperdevices/bridge/api/model/FlipperRequest.kt
@@ -7,22 +7,7 @@ data class FlipperRequest(
     val createTimestampNanos: Long = System.nanoTime(),
     val priority: FlipperRequestPriority = FlipperRequestPriority.DEFAULT,
     val onSendCallback: (() -> Unit)? = null
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as FlipperRequest
-
-        if (data != other.data) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return data.hashCode()
-    }
-}
+)
 
 fun Flipper.Main.wrapToRequest(
     priority: FlipperRequestPriority = FlipperRequestPriority.DEFAULT
