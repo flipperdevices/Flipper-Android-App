7afff7f7475d67f407782b8422b743ce450c1093
LionZXY <nikita@kulikof.ru>
2022-10-04 16:16:42 +0400

    Flipper widget part 1 (#399)
    **Background**
    
    Widgets require logic and design - I decided to break this down into two or more PRs.
    
    **Changes**
    
    - Add widget to new module
    - Add designs for widgets
    
    **Test plan**
    
    Try add widget to your homescreen

---
+++ components/widget/impl/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("flipper.lint")
+    id("flipper.android-lib")
+    id("com.squareup.anvil")
+    id("kotlin-kapt")
+}
+
+dependencies {
+    implementation(projects.components.core.di)
+    implementation(projects.components.core.ui.res)
+
+    // Dagger deps
+    implementation(libs.dagger)
+    kapt(libs.dagger.kapt)
+}
---
+++ components/widget/impl/src/main/java/com/flipperdevices/widget/impl/FlipperAppWidgetProvider.kt
@@ -0,0 +1,22 @@
+package com.flipperdevices.widget.impl
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+class FlipperAppWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        appWidgetIds.forEach { appWidgetId ->
+            appWidgetManager.updateAppWidget(
+                appWidgetId,
+                RemoteViews(context.packageName, R.layout.widget_layout_emulate)
+            )
+        }
+    }
+}
