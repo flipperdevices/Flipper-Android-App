0b676ace509cac2fba4dd35aab16f639331834d5
LionZXY <nikita@kulikof.ru>
2022-10-20 22:06:09 +0400

    Fix wearos

--- components/wearable/emulate/wear/impl/src/main/java/com/flipperdevices/wearable/emulate/impl/di/WearEmulateComponent.kt
+++ components/wearable/emulate/wear/impl/src/main/java/com/flipperdevices/wearable/emulate/impl/di/WearEmulateComponent.kt
@@ -3,8 +3,9 @@ package com.flipperdevices.wearable.emulate.impl.di
 import com.flipperdevices.core.di.AppGraph
 import com.flipperdevices.wearable.setup.api.SetupApi
 import com.squareup.anvil.annotations.ContributesTo
+import javax.inject.Provider
 
 @ContributesTo(AppGraph::class)
 interface WearEmulateComponent {
-    val setupApi: SetupApi
+    val setupApi: Provider<SetupApi>
 }
