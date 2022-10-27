4cce1d08fa696a9f8dfeae63ea29a0e6cb1f3390
LionZXY <nikita@kulikof.ru>
2022-10-27 22:43:23 +0400

    Flipper color on error screen

---
+++ components/nfc/mfkey32/screen/src/main/java/com/flipperdevices/nfc/mfkey32/screen/viewmodel/FlipperColorViewModel.kt
@@ -0,0 +1,26 @@
+package com.flipperdevices.nfc.mfkey32.screen.viewmodel
+
+import androidx.datastore.core.DataStore
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.flipperdevices.core.preference.pb.HardwareColor
+import com.flipperdevices.core.preference.pb.PairSettings
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import tangle.viewmodel.VMInject
+
+class FlipperColorViewModel @VMInject constructor(
+    settings: DataStore<PairSettings>
+) : ViewModel() {
+    private val colorFlipperState = MutableStateFlow(HardwareColor.WHITE)
+
+    init {
+        settings.data.onEach {
+            colorFlipperState.emit(it.hardwareColor)
+        }.launchIn(viewModelScope)
+    }
+
+    fun getFlipperColor(): StateFlow<HardwareColor> = colorFlipperState
+}
