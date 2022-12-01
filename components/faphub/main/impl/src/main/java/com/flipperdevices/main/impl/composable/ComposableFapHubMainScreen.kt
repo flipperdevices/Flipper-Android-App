a1a5ac0d76037721e2aa6c7696dd89a9aee6053b
LionZXY <nikita@kulikof.ru>
2022-12-01 17:44:09 +0400

    Add experimental switch

--- components/faphub/main/impl/src/main/java/com/flipperdevices/main/impl/composable/ComposableFapHubMainScreen.kt
+++ components/faphub/main/impl/src/main/java/com/flipperdevices/main/impl/composable/ComposableFapHubMainScreen.kt
@@ -2,6 +2,9 @@ package com.flipperdevices.main.impl.composable
 
 import com.flipperdevices.core.ui.res.R as DesignSystem
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -12,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.flipperdevices.core.ui.ktx.OrangeAppBarWithIcon
 import com.flipperdevices.main.impl.R
+import com.flipperdevices.main.impl.composable.switch.ComposableFapHubNewSwitch
 import com.flipperdevices.main.impl.composable.switch.ComposableFapHubSwitch
 import com.flipperdevices.main.impl.model.FapHubTabEnum
 import com.flipperdevices.main.impl.viewmodel.InstalledNotificationViewModel
@@ -31,23 +35,37 @@ fun ComposableFapHubMainScreen(
 
     val viewModel = tangleViewModel<MainViewModel>()
     val selectedTab by viewModel.getTabFlow().collectAsState()
+    val isNewBar by viewModel.isExperimentalSwitch().collectAsState()
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        OrangeAppBarWithIcon(
-            titleId = R.string.faphub_main_title,
-            onBack = onBack,
-            endIconId = DesignSystem.drawable.ic_search,
-            onEndClick = onOpenSearch
-        )
+        if (isNewBar) {
+            ComposableFapHubNewSwitch(
+                fapHubTabEnum = selectedTab,
+                onSelect = viewModel::onSelectTab,
+                installedNotificationCount = installedNotificationCount,
+                onBack = onBack,
+                onEndClick = onOpenSearch
+            )
+            Spacer(modifier = Modifier
+                .height(18.dp)
+                .fillMaxWidth())
+        } else {
+            OrangeAppBarWithIcon(
+                titleId = R.string.faphub_main_title,
+                onBack = onBack,
+                endIconId = DesignSystem.drawable.ic_search,
+                onEndClick = onOpenSearch
+            )
 
-        ComposableFapHubSwitch(
-            modifier = Modifier.padding(top = 6.dp, bottom = 18.dp),
-            fapHubTabEnum = selectedTab,
-            onSelect = viewModel::onSelectTab,
-            installedNotificationCount = installedNotificationCount
-        )
+            ComposableFapHubSwitch(
+                modifier = Modifier.padding(top = 6.dp, bottom = 18.dp),
+                fapHubTabEnum = selectedTab,
+                onSelect = viewModel::onSelectTab,
+                installedNotificationCount = installedNotificationCount
+            )
+        }
 
         when (selectedTab) {
             FapHubTabEnum.APPS -> catalogTabComposable()
--- components/faphub/main/impl/src/main/java/com/flipperdevices/main/impl/viewmodel/MainViewModel.kt
+++ components/faphub/main/impl/src/main/java/com/flipperdevices/main/impl/viewmodel/MainViewModel.kt
@@ -7,15 +7,26 @@ import com.flipperdevices.core.preference.pb.Settings
 import com.flipperdevices.main.impl.model.FapHubTabEnum
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import tangle.viewmodel.VMInject
 
 class MainViewModel @VMInject constructor(
-    private val settings: DataStore<Settings>,
+    settings: DataStore<Settings>,
 ) : ViewModel() {
     private val tabFlow = MutableStateFlow(FapHubTabEnum.APPS)
+    private val experimentalSwitchFlow = MutableStateFlow(false)
+
+    init {
+        settings.data.onEach {
+            experimentalSwitchFlow.emit(it.faphubNewSwitch)
+        }.launchIn(viewModelScope)
+    }
 
     fun getTabFlow(): StateFlow<FapHubTabEnum> = tabFlow
+
+    fun isExperimentalSwitch(): StateFlow<Boolean> = experimentalSwitchFlow
 
     fun onSelectTab(tabEnum: FapHubTabEnum) {
         viewModelScope.launch {
