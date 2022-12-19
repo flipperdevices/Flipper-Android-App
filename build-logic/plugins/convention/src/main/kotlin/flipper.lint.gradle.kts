a2daa809f01dc0c424a40ab35326427d8702d139
LionZXY <nikita@kulikof.ru>
2022-12-19 18:20:33 +0400

    Build failed on first detekt error, disable caching

--- build-logic/plugins/convention/src/main/kotlin/flipper.lint.gradle.kts
+++ build-logic/plugins/convention/src/main/kotlin/flipper.lint.gradle.kts
@@ -1,3 +1,4 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
 plugins {
@@ -6,7 +7,36 @@ plugins {
 
 configure<DetektExtension> {
     //allRules = true
+    parallel = true
+
     config = rootProject.files("config/detekt/detekt.yml")
+
+}
+
+tasks.register<Detekt>("detektFormat") {
+    autoCorrect = true
+}
+
+tasks.withType<Detekt> {
+    // Disable caching
+    outputs.upToDateWhen { false }
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        txt.required.set(false)
+    }
+
+    setSource(files(projectDir))
+
+    include("**/*.kt", "**/*.kts")
+    exclude(
+        "**/resources/**",
+        "**/build/**",
+    )
+
+    // Target version of the generated JVM bytecode. It is used for type resolution.
+    this.jvmTarget = "1.8"
 }
 
 dependencies {
