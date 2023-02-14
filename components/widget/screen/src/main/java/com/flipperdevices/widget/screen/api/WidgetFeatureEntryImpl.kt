230f4269ef27ddc61d45869b03e9e73769f59da0
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-02-14 11:16:11 +0200

    ComposableFeatureEntry for widget

--- components/widget/screen/src/main/java/com/flipperdevices/widget/screen/api/WidgetFeatureEntryImpl.kt
+++ components/widget/screen/src/main/java/com/flipperdevices/widget/screen/api/WidgetFeatureEntryImpl.kt
@@ -5,13 +5,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import com.flipperdevices.archive.api.ArchiveApi
 import com.flipperdevices.archive.api.SearchApi
 import com.flipperdevices.archive.api.SearchFeatureEntry
 import com.flipperdevices.bridge.dao.api.model.FlipperKeyPath
 import com.flipperdevices.core.di.AppGraph
-import com.flipperdevices.core.ui.navigation.AggregateFeatureEntry
+import com.flipperdevices.core.ui.navigation.ComposableFeatureEntry
 import com.flipperdevices.core.ui.navigation.GetOnceResult
 import com.flipperdevices.widget.api.WidgetFeatureEntry
 import com.flipperdevices.widget.screen.compose.WidgetOptionsComposable
@@ -24,16 +23,13 @@ import javax.inject.Inject
 internal const val EXTRA_WIDGET_ID_KEY = "widget_id"
 
 @ContributesBinding(AppGraph::class, WidgetFeatureEntry::class)
-@ContributesMultibinding(AppGraph::class, AggregateFeatureEntry::class)
+@ContributesMultibinding(AppGraph::class, ComposableFeatureEntry::class)
 class WidgetFeatureEntryImpl @Inject constructor(
     private val archiveApi: ArchiveApi,
     private val searchFeatureEntry: SearchFeatureEntry
 ) : WidgetFeatureEntry {
     override fun getWidgetScreen(widgetId: Int): String {
         return "@${ROUTE.name}?id=$widgetId"
-    }
-    private fun getWidgetScreenTemplate(): String {
-        return "@${ROUTE.name}?id={$EXTRA_WIDGET_ID_KEY}\","
     }
 
     private val widgetArguments = listOf(
@@ -43,28 +39,23 @@ class WidgetFeatureEntryImpl @Inject constructor(
         }
     )
 
-    override fun NavGraphBuilder.navigation(navController: NavHostController) {
-        navigation(
-            startDestination = getWidgetScreenTemplate(),
-            route = ROUTE.name
+    override fun NavGraphBuilder.composable(navController: NavHostController) {
+        composable(
+            route = "@${ROUTE.name}?id={$EXTRA_WIDGET_ID_KEY}",
+            arguments = widgetArguments
         ) {
-            composable(
-                route = getWidgetScreenTemplate(),
-                arguments = widgetArguments
-            ) {
-                val widgetSelectViewModel: WidgetSelectViewModel = tangleViewModel()
-                navController.GetOnceResult<FlipperKeyPath>(SearchApi.SEARCH_RESULT_KEY) {
-                    widgetSelectViewModel.onSelectKey(it)
-                }
-                WidgetOptionsComposable(
-                    archiveApi = archiveApi,
-                    widgetSelectViewModel = widgetSelectViewModel,
-                    onOpenSearchScreen = {
-                        val searchScreen = searchFeatureEntry.getSearchScreen(isExitOnOpenKey = true)
-                        navController.navigate(searchScreen)
-                    }
-                )
+            val widgetSelectViewModel: WidgetSelectViewModel = tangleViewModel()
+            navController.GetOnceResult<FlipperKeyPath>(SearchApi.SEARCH_RESULT_KEY) {
+                widgetSelectViewModel.onSelectKey(it)
             }
+            WidgetOptionsComposable(
+                archiveApi = archiveApi,
+                widgetSelectViewModel = widgetSelectViewModel,
+                onOpenSearchScreen = {
+                    val searchScreen = searchFeatureEntry.getSearchScreen(isExitOnOpenKey = true)
+                    navController.navigate(searchScreen)
+                }
+            )
         }
     }
 }
