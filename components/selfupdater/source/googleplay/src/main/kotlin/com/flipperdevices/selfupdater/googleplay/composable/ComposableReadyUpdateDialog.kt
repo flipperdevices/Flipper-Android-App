f177534dca96d38100d5c38179e73bffa7312af2
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-03-15 21:41:21 +0200

    google plau update dialog

--- components/selfupdater/source/googleplay/src/main/kotlin/com/flipperdevices/selfupdater/googleplay/composable/ComposableReadyUpdateDialog.kt
+++ components/selfupdater/source/googleplay/src/main/kotlin/com/flipperdevices/selfupdater/googleplay/composable/ComposableReadyUpdateDialog.kt
@@ -1,8 +1,10 @@
 package com.flipperdevices.selfupdater.googleplay.composable
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-import com.flipperdevices.core.ui.dialog.composable.FlipperDialog
+import com.flipperdevices.core.ui.dialog.composable.multichoice.FlipperMultiChoiceDialog
+import com.flipperdevices.core.ui.dialog.composable.multichoice.FlipperMultiChoiceDialogModel
 import com.flipperdevices.core.ui.theme.FlipperThemeInternal
 import com.flipperdevices.selfupdater.source.googleplay.R
 
@@ -11,13 +13,16 @@ internal fun ComposableReadyUpdateDialog(
     onAccept: () -> Unit,
     onDecline: () -> Unit,
 ) {
-    FlipperDialog(
-        titleId = R.string.ready_update_title,
-        textId = R.string.ready_update_text,
-        buttonTextId = R.string.ready_update_button,
-        onDismissRequest = onDecline,
-        onClickButton = onAccept
-    )
+    val dialogModel = remember(onDecline, onAccept) {
+        FlipperMultiChoiceDialogModel.Builder()
+            .setTitle(R.string.ready_update_title)
+            .setDescription(R.string.ready_update_text)
+            .setOnDismissRequest(onDecline)
+            .addButton(R.string.ready_update_button, onAccept, isActive = true)
+            .addButton(R.string.ready_update_cancel_button, onDecline)
+            .build()
+    }
+    FlipperMultiChoiceDialog(dialogModel)
 }
 
 @Preview
