017ed23a31471328f60b5681abf9fcb2027d0a89
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-04-10 12:17:42 +0300

    InAppNotification update redesign for font scale

--- components/inappnotification/impl/src/main/java/com/flipperdevices/inappnotification/impl/composable/type/ComposableInAppNotificationUpdateReady.kt
+++ components/inappnotification/impl/src/main/java/com/flipperdevices/inappnotification/impl/composable/type/ComposableInAppNotificationUpdateReady.kt
@@ -11,8 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.flipperdevices.core.ui.ktx.ComposableFlipperButton
 import com.flipperdevices.core.ui.theme.FlipperThemeInternal
 import com.flipperdevices.core.ui.theme.LocalTypography
@@ -28,7 +30,9 @@ internal fun ComposableInAppNotificationUpdateReady(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column {
+        Column(
+            modifier = Modifier.weight(2f)
+        ) {
             Text(
                 text = stringResource(R.string.ready_update_title),
                 style = LocalTypography.current.subtitleB12
@@ -39,11 +43,47 @@ internal fun ComposableInAppNotificationUpdateReady(
             )
         }
         ComposableFlipperButton(
-            textPadding = PaddingValues(vertical = 8.dp, horizontal = 18.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp),
+            textPadding = PaddingValues(vertical = 12.dp, horizontal = 4.dp),
             text = stringResource(R.string.ready_update_button),
+            textStyle = TextStyle(
+                fontSize = 12.sp
+            ),
             onClick = {
                 notification.action()
             }
+        )
+    }
+}
+
+@Preview(
+    fontScale = 2.0f
+)
+@Composable
+private fun ComposableInAppNotificationUpdateReadyPreviewX2Font() {
+    FlipperThemeInternal {
+        ComposableInAppNotificationUpdateReady(
+            notification = InAppNotification.UpdateReady(
+                durationMs = 0,
+                action = { },
+            )
+        )
+    }
+}
+
+@Preview(
+    fontScale = 1.5f
+)
+@Composable
+private fun ComposableInAppNotificationUpdateReadyPreviewX15Font() {
+    FlipperThemeInternal {
+        ComposableInAppNotificationUpdateReady(
+            notification = InAppNotification.UpdateReady(
+                durationMs = 0,
+                action = { },
+            )
         )
     }
 }
