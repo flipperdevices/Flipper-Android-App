660ba86064674f6dc12cbb7cd63d04913493f76a
TQMatvey <maybeetube@gmail.com>
2023-04-19 06:39:31 +0700

    Unleashify Settings menu

--- components/settings/impl/src/main/java/com/flipperdevices/settings/impl/composable/category/OtherSettingsCategory.kt
+++ components/settings/impl/src/main/java/com/flipperdevices/settings/impl/composable/category/OtherSettingsCategory.kt
@@ -1,26 +1,23 @@
 package com.flipperdevices.settings.impl.composable.category
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.flipperdevices.settings.impl.R
-import com.flipperdevices.settings.impl.composable.elements.ClickableElement
 import com.flipperdevices.settings.impl.composable.elements.GrayDivider
 import com.flipperdevices.settings.impl.composable.elements.UrlElement
 import com.flipperdevices.settings.impl.viewmodels.SettingsViewModel
 
 @Composable
 fun OtherSettingsCategory(
-    settingsViewModel: SettingsViewModel,
-    navController: NavController,
+    settingsViewModel: SettingsViewModel, // used in ComposableSettings
+    navController: NavController,         // used in ComposableSettings
 ) {
     CardCategory {
         UrlElement(
-            iconId = R.drawable.ic_forum,
-            titleId = R.string.other_forum_open,
-            url = stringResource(R.string.other_forum_url)
+            iconId = R.drawable.ic_telegram,
+            titleId = R.string.other_telegram_open,
+            url = stringResource(R.string.other_telegram_url)
         )
         GrayDivider()
         UrlElement(
@@ -29,15 +26,5 @@ fun OtherSettingsCategory(
             url = stringResource(R.string.other_github_url)
         )
         GrayDivider()
-        val s2rInitialized by settingsViewModel.getShake2ReportInitializationState()
-            .collectAsState()
-        if (!s2rInitialized) {
-            return@CardCategory
-        }
-        ClickableElement(
-            iconId = R.drawable.ic_bug,
-            titleId = R.string.other_shake2report_open,
-            onClick = { settingsViewModel.onReportBug(navController) }
-        )
     }
 }
