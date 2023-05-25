72236685743a05409cfc65553c44113b606b769f
Nikita Kulikov <nikita@kulikof.ru>
2023-05-25 13:56:37 +0400

    Rerun manifest fetch on fail

--- components/faphub/installation/manifest/impl/src/main/java/com/flipperdevices/faphub/installation/manifest/impl/api/FapManifestApiImpl.kt
+++ components/faphub/installation/manifest/impl/src/main/java/com/flipperdevices/faphub/installation/manifest/impl/api/FapManifestApiImpl.kt
@@ -4,6 +4,7 @@ import com.flipperdevices.core.di.AppGraph
 import com.flipperdevices.core.ktx.jre.launchWithLock
 import com.flipperdevices.core.ktx.jre.withLock
 import com.flipperdevices.core.log.LogTagProvider
+import com.flipperdevices.core.log.error
 import com.flipperdevices.faphub.installation.manifest.api.FapManifestApi
 import com.flipperdevices.faphub.installation.manifest.impl.utils.FapManifestUploader
 import com.flipperdevices.faphub.installation.manifest.impl.utils.FapManifestsLoader
@@ -13,9 +14,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.sync.Mutex
+import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -30,12 +33,14 @@ class FapManifestApiImpl @Inject constructor(
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     private val mutex = Mutex()
     private val fapManifestItemFlow = MutableStateFlow<List<FapManifestItem>?>(null)
+    private val shouldInvalidate = AtomicBoolean(true)
 
-    init {
-        invalidate()
+    override fun getManifestFlow(): StateFlow<List<FapManifestItem>?> {
+        if (shouldInvalidate.compareAndSet(true, false)) {
+            invalidateAsync()
+        }
+        return fapManifestItemFlow.asStateFlow()
     }
-
-    override fun getManifestFlow() = fapManifestItemFlow.asStateFlow()
 
     override suspend fun add(
         pathToFap: String,
@@ -47,7 +52,14 @@ class FapManifestApiImpl @Inject constructor(
         }
     }
 
-    private fun invalidate() = launchWithLock(mutex, scope, "invalidate") {
-        fapManifestItemFlow.emit(loader.load())
+    private fun invalidateAsync() = launchWithLock(mutex, scope, "invalidate") {
+        runCatching {
+            loader.load()
+        }.onFailure {
+            error(it) { "Failed load manifests" }
+            shouldInvalidate.compareAndSet(false, true)
+        }.onSuccess {
+            fapManifestItemFlow.emit(it)
+        }
     }
 }
