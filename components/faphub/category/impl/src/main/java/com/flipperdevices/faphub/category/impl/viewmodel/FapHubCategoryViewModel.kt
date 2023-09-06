e31028c6974625f104fb0c5efe16393e915ca53f
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-09-06 16:23:39 +0300

    review

--- components/faphub/category/impl/src/main/java/com/flipperdevices/faphub/category/impl/viewmodel/FapHubCategoryViewModel.kt
+++ components/faphub/category/impl/src/main/java/com/flipperdevices/faphub/category/impl/viewmodel/FapHubCategoryViewModel.kt
@@ -7,21 +7,21 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import com.flipperdevices.bridge.dao.api.FapHubHideItemApi
+import com.flipperdevices.core.ktx.jre.map
 import com.flipperdevices.core.pager.loadingPagingDataFlow
-import com.flipperdevices.core.preference.pb.SelectedCatalogSort
 import com.flipperdevices.core.preference.pb.Settings
 import com.flipperdevices.faphub.category.impl.api.CATEGORY_OPEN_PATH_KEY
 import com.flipperdevices.faphub.dao.api.FapNetworkApi
 import com.flipperdevices.faphub.dao.api.model.FapCategory
 import com.flipperdevices.faphub.dao.api.model.SortType
+import com.flipperdevices.faphub.dao.api.model.SortType.Companion.toSortType
 import com.flipperdevices.faphub.target.api.FlipperTargetProviderApi
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import tangle.inject.TangleParam
 import tangle.viewmodel.VMInject
@@ -34,19 +34,20 @@ class FapHubCategoryViewModel @VMInject constructor(
     targetProviderApi: FlipperTargetProviderApi,
     private val dataStoreSettings: DataStore<Settings>
 ) : ViewModel() {
-    private val sortTypeFlow = MutableStateFlow(SortType.UPDATE_AT_DESC)
-    fun getSortTypeFlow(): StateFlow<SortType> = sortTypeFlow.asStateFlow()
+    private val settingsState by lazy {
+        dataStoreSettings.data.stateIn(
+            viewModelScope,
+            SharingStarted.Lazily,
+            initialValue = Settings.getDefaultInstance()
+        )
+    }
 
-    init {
-        viewModelScope.launch {
-            dataStoreSettings.data.collectLatest {
-                sortTypeFlow.emit(it.selectedCatalogSort.toSortType())
-            }
-        }
+    fun getSortTypeFlow(): StateFlow<SortType> = settingsState.map(viewModelScope) {
+        it.selectedCatalogSort.toSortType()
     }
 
     val faps = combine(
-        sortTypeFlow,
+        getSortTypeFlow(),
         targetProviderApi.getFlipperTarget(),
         fapHubHideItemApi.getHiddenItems()
     ) { sortType, target, hiddenItems ->
@@ -71,23 +72,4 @@ class FapHubCategoryViewModel @VMInject constructor(
             }
         }
     }
-}
-
-private fun SelectedCatalogSort.toSortType(): SortType = when (this) {
-    SelectedCatalogSort.UPDATE_AT_DESC -> SortType.UPDATE_AT_DESC
-    SelectedCatalogSort.UPDATE_AT_ASC -> SortType.UPDATE_AT_ASC
-    SelectedCatalogSort.CREATED_AT_DESC -> SortType.CREATED_AT_DESC
-    SelectedCatalogSort.CREATED_AT_ASC -> SortType.CREATED_AT_ASC
-    SelectedCatalogSort.NAME_DESC -> SortType.NAME_DESC
-    SelectedCatalogSort.NAME_ASC -> SortType.NAME_ASC
-    SelectedCatalogSort.UNRECOGNIZED -> SortType.UPDATE_AT_DESC
-}
-
-private fun SortType.toSelectedSortType(): SelectedCatalogSort = when (this) {
-    SortType.UPDATE_AT_DESC -> SelectedCatalogSort.UPDATE_AT_DESC
-    SortType.UPDATE_AT_ASC -> SelectedCatalogSort.UPDATE_AT_ASC
-    SortType.CREATED_AT_DESC -> SelectedCatalogSort.CREATED_AT_DESC
-    SortType.CREATED_AT_ASC -> SelectedCatalogSort.CREATED_AT_ASC
-    SortType.NAME_DESC -> SelectedCatalogSort.NAME_DESC
-    SortType.NAME_ASC -> SelectedCatalogSort.NAME_ASC
 }
