1a9c2bb79ab5eacae2794a1347550c1bb71bb4f4
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2023-11-03 11:24:08 +0200

    UI Optimisation in appllication catalog (#726)
    **Background**
    
    Now we have some unstable compose function
    
    **Changes**
    
    * Run compose metrics
    * Fix Stable and Immutable value
    
    **Test plan**
    
    Try use catalog

--- components/bottombar/api/src/main/java/com/flipperdevices/bottombar/api/BottomNavigationHandleDeeplink.kt
+++ components/bottombar/api/src/main/java/com/flipperdevices/bottombar/api/BottomNavigationHandleDeeplink.kt
@@ -1,8 +1,10 @@
 package com.flipperdevices.bottombar.api
 
 import android.content.Intent
+import androidx.compose.runtime.Immutable
 import com.flipperdevices.bottombar.model.BottomBarTab
 
+@Immutable
 interface BottomNavigationHandleDeeplink {
     fun handleDeepLink(intent: Intent)
 
