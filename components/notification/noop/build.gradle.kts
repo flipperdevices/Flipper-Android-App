d2e4cffd71d5fe07b76a43d4569a7e3f7a593d2a
Nikita Kulikov <nikita@kulikof.ru>
2023-11-10 17:19:18 +0000

    Fix noop impl

--- components/notification/noop/build.gradle.kts
+++ components/notification/noop/build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
-    id("flipper.android-lib")
+    id("flipper.android-compose")
+    id("flipper.anvil")
 }
 
 android.namespace = "com.flipperdevices.notification.noop"
 
 dependencies {
+    implementation(projects.components.core.di)
+
     implementation(projects.components.notification.api)
+    implementation(projects.components.inappnotification.api)
+
+    // Compose
+    implementation(libs.compose.ui)
+
+    implementation(libs.kotlin.coroutines)
 }
