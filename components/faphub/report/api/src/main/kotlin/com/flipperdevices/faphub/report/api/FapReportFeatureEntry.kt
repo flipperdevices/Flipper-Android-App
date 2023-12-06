f36bf9ea495298778f9058ded73a22fc147522f9
Nikita Kulikov <nikita@kulikof.ru>
2023-12-06 08:31:33 +0000

    Add two screens to report bug sections

--- components/faphub/report/api/src/main/kotlin/com/flipperdevices/faphub/report/api/FapReportFeatureEntry.kt
+++ components/faphub/report/api/src/main/kotlin/com/flipperdevices/faphub/report/api/FapReportFeatureEntry.kt
@@ -1,11 +1,11 @@
 package com.flipperdevices.faphub.report.api
 
-import com.flipperdevices.core.ui.navigation.ComposableFeatureEntry
+import com.flipperdevices.core.ui.navigation.AggregateFeatureEntry
 import com.flipperdevices.core.ui.navigation.FeatureScreenRootRoute
 
-interface FapReportFeatureEntry : ComposableFeatureEntry {
+interface FapReportFeatureEntry : AggregateFeatureEntry {
     override val ROUTE: FeatureScreenRootRoute
         get() = FeatureScreenRootRoute.FAP_HUB_REPORT
 
-    fun start(applicationUid: String): String
+    fun start(applicationUid: String, reportUrl: String): String
 }
