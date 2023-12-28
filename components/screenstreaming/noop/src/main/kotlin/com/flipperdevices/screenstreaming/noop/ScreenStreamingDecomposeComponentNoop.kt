fb583f26378bfd61be901fdc0d54cbb8bb38891e
Nikita Kulikov <nikita@kulikof.ru>
2023-12-28 15:05:53 +0000

    Fix noop

---
+++ components/screenstreaming/noop/src/main/kotlin/com/flipperdevices/screenstreaming/noop/ScreenStreamingDecomposeComponentNoop.kt
@@ -0,0 +1,27 @@
+package com.flipperdevices.screenstreaming.noop
+
+import androidx.compose.runtime.Composable
+import com.arkivanov.decompose.ComponentContext
+import com.flipperdevices.core.di.AppGraph
+import com.flipperdevices.screenstreaming.api.ScreenStreamingDecomposeComponent
+import com.flipperdevices.ui.decompose.DecomposeOnBackParameter
+import com.squareup.anvil.annotations.ContributesBinding
+import javax.inject.Inject
+
+class ScreenStreamingDecomposeComponentNoop @Inject constructor() :
+    ScreenStreamingDecomposeComponent {
+
+
+    @Composable
+    override fun Render() {
+        // Noop
+    }
+
+    @ContributesBinding(AppGraph::class, ScreenStreamingDecomposeComponent.Factory::class)
+    interface Factory : ScreenStreamingDecomposeComponent.Factory {
+        override fun invoke(
+            componentContext: ComponentContext,
+            onBack: DecomposeOnBackParameter
+        ): ScreenStreamingDecomposeComponentNoop
+    }
+}
