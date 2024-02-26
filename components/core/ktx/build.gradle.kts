0665e101068d8fb445065b2a5d0a43febfa68475
Nikita Kulikov <nikita@kulikof.ru>
2024-02-26 11:25:55 +0000

    [WIP] Migrate core ktx

--- components/core/ktx/build.gradle.kts
+++ components/core/ktx/build.gradle.kts
@@ -7,24 +7,14 @@ android.namespace = "com.flipperdevices.core.ktx"
 kotlin {
     sourceSets {
         commonMain.dependencies {
+            implementation(projects.components.core.log)
+            implementation(libs.kotlin.coroutines)
         }
         androidMain.dependencies {
-            implementation(projects.components.core.log)
-
             implementation(libs.appcompat)
-            implementation(libs.kotlin.coroutines)
-
-            implementation(libs.kotlin.coroutines)
-
-            // Compose
-            implementation(libs.compose.ui)
-            implementation(libs.compose.tooling)
-            implementation(libs.compose.foundation)
-            implementation(libs.compose.material)
         }
-        /*commonTest.dependencies {
-            implementation(projects.components.core.test)
+        commonTest.dependencies {
             implementation(libs.junit)
-        }*/
+        }
     }
 }
