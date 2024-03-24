1fd181618f5b6628c3aa66b063dfca91368c99fc
Timur Zakirov <tumyp164@gmail.com>
2024-03-24 19:09:21 +0000

    Switch Mockito on mockk and update changelog

--- components/core/progress/src/test/java/com/flipperdevices/core/progress/ProgressWrapperTrackerTest.kt
+++ components/core/progress/src/test/java/com/flipperdevices/core/progress/ProgressWrapperTrackerTest.kt
@@ -2,11 +2,11 @@ package com.flipperdevices.core.progress
 
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import io.mockk.mockk
 
 class ProgressWrapperTrackerTest {
 
