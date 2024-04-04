55aba17c80248128a2936862323514bc88499bea
Roman Makeev <makeevrserg@gmail.com>
2024-04-04 18:25:23 +0300

    Fix font on ComposableIconText

--- components/infrared/impl/src/main/kotlin/com/flipperdevices/infrared/impl/composable/components/ComposableIconText.kt
+++ components/infrared/impl/src/main/kotlin/com/flipperdevices/infrared/impl/composable/components/ComposableIconText.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.flipperdevices.core.ui.theme.FlipperThemeInternal
 import com.flipperdevices.core.ui.theme.LocalPallet
 import com.flipperdevices.core.ui.theme.LocalTypography
@@ -54,7 +53,7 @@ internal fun ComposableIconText(
         }
         Text(
             text = text,
-            style = LocalTypography.current.bodyR14.copy(fontSize = 12.sp),
+            style = LocalTypography.current.subtitleM12,
             color = LocalPallet.current.text30
         )
     }
