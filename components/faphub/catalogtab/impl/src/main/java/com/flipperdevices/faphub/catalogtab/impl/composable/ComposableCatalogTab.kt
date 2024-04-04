d024906ae60a2569be429b3847044b80974b409a
Roman Makeev <makeevrserg@gmail.com>
2024-04-04 20:18:08 +0300

    Add remember to FapHubError, remove overhead from ComposableFullScreenError

--- components/faphub/catalogtab/impl/src/main/java/com/flipperdevices/faphub/catalogtab/impl/composable/ComposableCatalogTab.kt
+++ components/faphub/catalogtab/impl/src/main/java/com/flipperdevices/faphub/catalogtab/impl/composable/ComposableCatalogTab.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.paging.LoadState
@@ -21,6 +22,9 @@ import com.flipperdevices.faphub.dao.api.model.FapCategory
 import com.flipperdevices.faphub.dao.api.model.FapItemShort
 import com.flipperdevices.faphub.errors.api.FapErrorSize
 import com.flipperdevices.faphub.errors.api.FapHubComposableErrorsRenderer
+import com.flipperdevices.faphub.errors.api.throwable.toFapHubError
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.map
 
 @Composable
 fun ComposableCatalogTabScreen(
@@ -35,6 +39,12 @@ fun ComposableCatalogTabScreen(
     val fapsList = fapsListViewModel.getFapsFlow().collectAsLazyPagingItems()
     val sortType by fapsListViewModel.getSortTypeFlow().collectAsState()
     val categoriesLoadState by categoriesViewModel.getCategoriesLoadState().collectAsState()
+
+    val categoriesFapHubError by remember {
+        categoriesViewModel.getCategoriesLoadState()
+            .filterIsInstance<CategoriesLoadState.Error>()
+            .map { categoriesErrorLoadState -> categoriesErrorLoadState.throwable.toFapHubError() }
+    }.collectAsState(initial = null)
 
     val refreshAll = {
         fapsListViewModel.refreshManifest()
@@ -66,14 +76,16 @@ fun ComposableCatalogTabScreen(
                 errorsRenderer = errorsRenderer,
                 installationButton = installationButton,
                 defaultFapErrorSize = FapErrorSize.IN_LIST,
-                shouldDisplayError = categoriesLoadState !is CategoriesLoadState.Error
+                shouldDisplayError = categoriesFapHubError == null
             )
 
-            ComposableFullScreenError(
-                categoriesLoadState = categoriesLoadState,
-                errorsRenderer = errorsRenderer,
-                onRetry = refreshAll
-            )
+            categoriesFapHubError?.let { categoriesFapHubError ->
+                ComposableFullScreenError(
+                    fapHubError = categoriesFapHubError,
+                    errorsRenderer = errorsRenderer,
+                    onRetry = refreshAll
+                )
+            }
         }
     }
 }
--- components/faphub/catalogtab/impl/src/main/java/com/flipperdevices/faphub/catalogtab/impl/composable/categories/ComposableFullScreenError.kt
+++ components/faphub/catalogtab/impl/src/main/java/com/flipperdevices/faphub/catalogtab/impl/composable/categories/ComposableFullScreenError.kt
@@ -2,29 +2,23 @@ package com.flipperdevices.faphub.catalogtab.impl.composable.categories
 
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.ui.Modifier
-import com.flipperdevices.faphub.catalogtab.impl.model.CategoriesLoadState
 import com.flipperdevices.faphub.errors.api.FapErrorSize
 import com.flipperdevices.faphub.errors.api.FapHubComposableErrorsRenderer
-import com.flipperdevices.faphub.errors.api.throwable.toFapHubError
+import com.flipperdevices.faphub.errors.api.throwable.FapHubError
 
 @Suppress("FunctionNaming")
 internal fun LazyListScope.ComposableFullScreenError(
-    categoriesLoadState: CategoriesLoadState,
+    fapHubError: FapHubError,
     errorsRenderer: FapHubComposableErrorsRenderer,
     onRetry: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    (categoriesLoadState as? CategoriesLoadState.Error)
-        ?.throwable
-        ?.toFapHubError()
-        ?.let { fapHubError ->
-            with(errorsRenderer) {
-                ComposableThrowableErrorListItem(
-                    modifier = modifier,
-                    throwable = fapHubError,
-                    onRetry = onRetry,
-                    fapErrorSize = FapErrorSize.FULLSCREEN
-                )
-            }
-        }
+    with(errorsRenderer) {
+        ComposableThrowableErrorListItem(
+            modifier = modifier,
+            throwable = fapHubError,
+            onRetry = onRetry,
+            fapErrorSize = FapErrorSize.FULLSCREEN
+        )
+    }
 }
