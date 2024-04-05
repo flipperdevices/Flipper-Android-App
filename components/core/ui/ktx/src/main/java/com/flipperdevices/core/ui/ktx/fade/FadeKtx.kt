816f52a12b360df16821697db67d678e89245ebe
Roman Makeev <makeevrserg@gmail.com>
2024-04-05 14:40:43 +0300

    Fix composed modifier for fading edge, add immutable, stable annotations

--- components/core/ui/ktx/src/main/java/com/flipperdevices/core/ui/ktx/fade/FadeKtx.kt
+++ components/core/ui/ktx/src/main/java/com/flipperdevices/core/ui/ktx/fade/FadeKtx.kt
@@ -1,89 +1,67 @@
 package com.flipperdevices.core.ui.ktx.fade
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.flipperdevices.core.ui.theme.FlipperThemeInternal
+import androidx.compose.ui.node.DrawModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.platform.InspectorInfo
 
-fun Modifier.fadingEdge(orientation: FadeOrientation): Modifier = composed(
-    factory = {
-        val fillColor = Color.White
-        val brush = remember { orientation.toBrush(fillColor) }
-        this.fadingEdge(brush)
-    }
-)
+@Stable
+fun Modifier.fadingEdge(orientation: FadeOrientation): Modifier {
+    return this
+        .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+        .then(FadingEdgeElement(orientation))
+}
 
-private fun FadeOrientation.toBrush(fillColor: Color) = when (this) {
-    is FadeOrientation.Bottom -> {
-        Brush.verticalGradient(
-            0f to fillColor,
-            this.THRESHOLD to fillColor,
-            1f to Color.Transparent
-        )
+private data class FadingEdgeElement(
+    private val orientation: FadeOrientation
+) : ModifierNodeElement<FadingEdgeNode>() {
+
+    override fun create(): FadingEdgeNode {
+        return FadingEdgeNode(orientation)
     }
 
-    is FadeOrientation.Top -> {
-        Brush.verticalGradient(
-            0f to Color.Transparent,
-            this.THRESHOLD to fillColor
-        )
+    override fun update(node: FadingEdgeNode) {
+        node.orientation = orientation
+    }
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "FadingEdge"
+        properties["orientation"] = orientation.name
     }
 }
 
-private fun Modifier.fadingEdge(brush: Brush) = this
-    .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
-    .drawWithContent {
+private class FadingEdgeNode(var orientation: FadeOrientation) : Modifier.Node(), DrawModifierNode {
+    private fun FadeOrientation.toBrush() = when (this) {
+        is FadeOrientation.Bottom -> {
+            Brush.verticalGradient(
+                0f to FILL_COLOR,
+                this.THRESHOLD to FILL_COLOR,
+                1f to Color.Transparent
+            )
+        }
+
+        is FadeOrientation.Top -> {
+            Brush.verticalGradient(
+                0f to Color.Transparent,
+                this.THRESHOLD to FILL_COLOR
+            )
+        }
+    }
+
+    override fun ContentDrawScope.draw() {
+        val brush = orientation.toBrush()
         drawContent()
         drawRect(brush = brush, blendMode = BlendMode.DstIn)
     }
 
-@Composable
-private fun FadePreviewColumn(
-    itemsAmount: Int,
-    modifier: Modifier = Modifier
-) {
-    Column(modifier = modifier) {
-        repeat(itemsAmount) {
-            Box(
-                modifier = Modifier
-                    .size(24.dp)
-                    .background(Color.Red)
-            )
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun TopFadePreview() {
-    FlipperThemeInternal {
-        FadePreviewColumn(
-            modifier = Modifier.fadingEdge(FadeOrientation.Top),
-            itemsAmount = 4
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun BottomFadePreview() {
-    FlipperThemeInternal {
-        FadePreviewColumn(
-            modifier = Modifier.fadingEdge(FadeOrientation.Bottom),
-            itemsAmount = 4
-        )
+    companion object {
+        private val FILL_COLOR = Color.White
     }
 }
--- components/core/ui/ktx/src/main/java/com/flipperdevices/core/ui/ktx/fade/FadeOrientation.kt
+++ components/core/ui/ktx/src/main/java/com/flipperdevices/core/ui/ktx/fade/FadeOrientation.kt
@@ -1,11 +1,22 @@
 package com.flipperdevices.core.ui.ktx.fade
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 sealed interface FadeOrientation {
+    val name: String
+
+    @Immutable
     data object Bottom : FadeOrientation {
+        override val name: String = "Bottom"
+
         const val THRESHOLD: Float = DEFAULT_FADE_THRESHOLD
     }
 
+    @Immutable
     data object Top : FadeOrientation {
+        override val name: String = "Top"
+
         const val THRESHOLD: Float = DEFAULT_FADE_THRESHOLD
     }
 
