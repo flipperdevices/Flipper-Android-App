1bff98df32e19d3529e4e114a526dda6a2a860d1
Roman Makeev <makeevrserg@gmail.com>
2024-04-10 09:09:21 +0300

    Share bitmap from url

---
+++ components/faphub/screenshotspreview/impl/src/main/kotlin/com/flipperdevices/faphub/screenshotspreview/impl/viewmodel/UrlImageShareViewModel.kt
@@ -0,0 +1,69 @@
+package com.flipperdevices.faphub.screenshotspreview.impl.viewmodel
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import com.flipperdevices.core.ktx.jre.createClearNewFileWithMkDirs
+import com.flipperdevices.core.share.SharableFile
+import com.flipperdevices.core.share.ShareHelper
+import com.flipperdevices.core.ui.lifecycle.DecomposeViewModel
+import com.flipperdevices.faphub.screenshotspreview.impl.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import javax.inject.Inject
+
+private const val SCREENSHOT_FILE_PREFIX = "flpr"
+private const val TIMEFORMAT = "yyyy-MM-dd-HH:mm:ss"
+private const val QUALITY = 100
+private const val ACCENT_COLOR_R = 255
+private const val ACCENT_COLOR_G = 130
+private const val ACCENT_COLOR_B = 0
+
+class UrlImageShareViewModel @Inject constructor(
+    private val applicationContext: Context
+) : DecomposeViewModel() {
+
+    private fun shareScreenshot(bitmap: Bitmap) {
+        val date = SimpleDateFormat(TIMEFORMAT, Locale.US).format(Date())
+        val filename = "$SCREENSHOT_FILE_PREFIX-$date.png"
+        val sharableFile = SharableFile(applicationContext, filename)
+        sharableFile.createClearNewFileWithMkDirs()
+        sharableFile.outputStream().use { fis ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, QUALITY, fis)
+        }
+        ShareHelper.shareFile(
+            applicationContext,
+            sharableFile,
+            R.string.screenshotspreview_export_title
+        )
+    }
+
+    private fun URL.decodeBitmap(): Result<Bitmap> {
+        return kotlin.runCatching {
+            openStream().use(BitmapFactory::decodeStream)
+        }
+    }
+
+    fun shareUrlImage(url: URL) = viewModelScope.launch(Dispatchers.IO) {
+        url.decodeBitmap()
+            .map { bitmap -> bitmap.fillBackground() }
+            .onSuccess { bitmap -> withContext(Dispatchers.Main) { shareScreenshot(bitmap) } }
+    }
+
+    // Images have transparent background thus we need to fill it with accent color
+    private fun Bitmap.fillBackground(): Bitmap {
+        val newBitmap = Bitmap.createBitmap(width, height, config)
+        val canvas = Canvas(newBitmap)
+        canvas.drawColor(Color.rgb(ACCENT_COLOR_R, ACCENT_COLOR_G, ACCENT_COLOR_B))
+        canvas.drawBitmap(this, 0F, 0F, null)
+        recycle()
+        return newBitmap
+    }
+}
