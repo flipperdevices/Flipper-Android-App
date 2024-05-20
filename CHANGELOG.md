03790b9f488b4d71475c16a032cf714b342b5f52
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2024-05-20 15:24:48 +0300

    Fix/mfkey32 initial progress state (#854)
    **Background**
    
    When open NFC Tools -> MIFARE Classic, it will immediately start
    downloading even if folder is empty
    
    **Changes**
    
    - Ad check for storage content
    
    **Test plan**
    
    - Open Mfkey32 (Detect Reader)
    - See that loading now will not start if storage is empty

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -37,6 +37,7 @@
 - [FIX] Text overflow on apps card
 - [FIX] SD Card error on installed screen
 - [FIX] Scrolling of zoomed screenshots in app image preview
+- [FIX] Display MfKey32 loading progress when no keys available 
 - [Feature] Add not connected, empty and syncing states to emulation button on key screen
 - [Feature] Check app exist on apps catalog manifest loading
 - [Feature] First version of device orchestrator
--- components/nfc/mfkey32/api/src/main/java/com/flipperdevices/nfc/mfkey32/api/MfKey32Api.kt
+++ components/nfc/mfkey32/api/src/main/java/com/flipperdevices/nfc/mfkey32/api/MfKey32Api.kt
@@ -4,6 +4,13 @@ import com.flipperdevices.bridge.api.manager.FlipperRequestApi
 import kotlinx.coroutines.flow.Flow
 
 interface MfKey32Api {
+    /**
+     * Cached value of bruteforce file existing
+     *
+     * This should be updated each [checkBruteforceFileExist]
+     */
+    val isBruteforceFileExist: Boolean
+
     fun hasNotification(): Flow<Boolean>
     suspend fun checkBruteforceFileExist(requestApi: FlipperRequestApi)
 }
