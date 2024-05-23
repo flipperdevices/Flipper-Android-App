d1d56a158d3d603f84a47e34d1fb8bbebd08f0c3
Roman Makeev <makeevrserg@gmail.com>
2024-05-23 15:09:28 +0300

    Add javadoc to explain why sharedJvmMain named like that

--- build-logic/plugins/convention/src/main/kotlin/flipper.multiplatform-compose.gradle.kts
+++ build-logic/plugins/convention/src/main/kotlin/flipper.multiplatform-compose.gradle.kts
@@ -21,13 +21,21 @@ kotlin {
     jvm("desktop")
 
     sourceSets {
-        val jvmMain by creating
+        /**
+         * We shouldn't create sources, which is named as original targets sourcesets.
+         *
+         * As an example - jvm() target will create sourceSet jvmMain - and we would have conflicts
+         * with our create jvmMain sourceSet
+         *
+         * This is the reason to name it `sharedJvmMain`
+         */
+        val sharedJvmMain by creating
 
         val androidMain by getting {
-            dependsOn(jvmMain)
+            dependsOn(sharedJvmMain)
         }
         val desktopMain by getting {
-            dependsOn(jvmMain)
+            dependsOn(sharedJvmMain)
         }
 
         androidMain.dependencies {
