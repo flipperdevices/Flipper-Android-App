d3b875016f29987155f4e60959475647c5dbceb5
Nikita Kulikov <n@kulikov.uk>
2024-05-27 11:49:38 +0100

    Add compose plugin to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,5 @@ plugins {
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.baselineprofile) apply false
     id("flipper.lint")
-    id("flipper.android-app") apply false
-    id("flipper.android-compose") apply false
-    id("flipper.android-lib") apply false
-    id("flipper.anvil") apply false
-    id("flipper.anvil.kapt") apply false
-    id("flipper.multiplatform") apply false
-    id("flipper.multiplatform-compose") apply false
+    alias(libs.plugins.compose) apply false
 }
