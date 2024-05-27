4bc0603f972c44644d2733a26cb6b61ea38ebbd9
makeevrserg <makeevrserg@gmail.com>
2024-05-27 16:27:10 +0300

    fix tests run

--- build-logic/plugins/convention/src/main/kotlin/flipper.android-lib.gradle.kts
+++ build-logic/plugins/convention/src/main/kotlin/flipper.android-lib.gradle.kts
@@ -8,16 +8,3 @@ plugins {
 configure<BaseExtension> {
     commonAndroid(project)
 }
-
-/**
- * KMP tasks are using allTests to run tests
- *
- * When KMP setup is not available at flipper.android-lib, we can create here custom task
- * which will run testDebugUnitTest
- */
-tasks.findByName("testDebug")?.let { androidUnitTests ->
-    tasks.register("allTests") {
-        group = LifecycleBasePlugin.VERIFICATION_GROUP
-        dependsOn(androidUnitTests)
-    }
-}
