1d62c3fb14512384180f4093c09de9b357b75796
makeevrserg <makeevrserg@gmail.com>
2024-05-27 21:16:28 +0300

    update FlavorType, add javadoc

--- components/core/build-konfig/build.gradle.kts
+++ components/core/build-konfig/build.gradle.kts
@@ -1,6 +1,4 @@
 import com.flipperdevices.buildlogic.ApkConfig.CURRENT_FLAVOR_TYPE
-import com.flipperdevices.buildlogic.model.FlavorType
-import com.github.gmazzo.buildconfig.BuildConfigExtension
 
 plugins {
     id("flipper.multiplatform")
@@ -12,19 +10,10 @@ group = "com.flipperdevices.core.buildkonfig"
 
 android.namespace = "$group"
 
-fun BuildConfigExtension.buildConfigIsLogEnabledField() {
-    val key = "IS_LOG_ENABLED"
-    buildConfigField(
-        type = Boolean::class.java,
-        name = key,
-        value = provider { CURRENT_FLAVOR_TYPE == FlavorType.DEV }
-    )
-}
-
 buildConfig {
     className("BuildKonfig")
     packageName("$group")
     useKotlinOutput { internalVisibility = false }
     buildConfigField(String::class.java, "PACKAGE", "$group")
-    buildConfigIsLogEnabledField()
+    buildConfigField(Boolean::class.java, "IS_LOG_ENABLED", CURRENT_FLAVOR_TYPE.isLogEnabled)
 }
