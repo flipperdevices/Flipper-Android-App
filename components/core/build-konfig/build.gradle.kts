8afed692ed313072399866fdb129d9e6da89d6d6
makeevrserg <makeevrserg@gmail.com>
2024-05-27 12:25:57 +0300

    move is log enabled field out of buildconfig

--- components/core/build-konfig/build.gradle.kts
+++ components/core/build-konfig/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.gmazzo.buildconfig.BuildConfigExtension
+
 plugins {
     id("flipper.multiplatform")
     id("flipper.multiplatform-dependencies")
@@ -12,22 +14,29 @@ commonDependencies {
     implementation(libs.timber)
 }
 
+/**
+ * This value can be placed inside gradle.properties `IS_LOG_ENABLED=false` or passed
+ * via ci as ORG_GRADLE_PROJECT_IS_LOG_ENABLED
+ */
+fun BuildConfigExtension.buildConfigIsLogEnabledField() {
+    val key = "IS_LOG_ENABLED"
+    buildConfigField(
+        type = Boolean::class.java,
+        name = key,
+        value = provider {
+            val isLogEnabled = providers.gradleProperty(key).orNull
+            if (isLogEnabled == null) {
+                logger.warn("Key $key is not present in gradle.properties or Environment")
+            }
+            isLogEnabled ?: true
+        }
+    )
+}
+
 buildConfig {
     className("BuildKonfig")
     packageName("$group")
     useKotlinOutput { internalVisibility = false }
     buildConfigField(String::class.java, "PACKAGE", "$group")
-    "IS_LOG_ENABLED".let { key ->
-        buildConfigField(
-            type = Boolean::class.java,
-            name = key,
-            value = provider {
-                val isLogEnabled = providers.gradleProperty(key).orNull
-                if (isLogEnabled == null) {
-                    logger.warn("Key $key is not present in gradle.properties or Environment")
-                }
-                isLogEnabled ?: true
-            }
-        )
-    }
+    buildConfigIsLogEnabledField()
 }
