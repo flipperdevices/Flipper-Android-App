5ebbea1d10d73a6dd674d425df92836585b532e5
Nikita Kulikov <nikita@kulikof.ru>
2024-06-07 11:35:52 +0100

    Change update button for respect ripple area

--- components/changelog/impl/src/main/kotlin/com/flipperdevices/changelog/impl/composable/ChangelogUpdateButtonComposable.kt
+++ components/changelog/impl/src/main/kotlin/com/flipperdevices/changelog/impl/composable/ChangelogUpdateButtonComposable.kt
@@ -1,11 +1,13 @@
 package com.flipperdevices.changelog.impl.composable
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
@@ -29,27 +31,32 @@ fun ChangelogUpdateButtonComposable(
     } else {
         LocalPalletV2.current.action.fwInstall.background.primary.default
     }
-    Text(
+
+    Box(
         modifier = modifier
-            .padding(horizontal = 14.dp, vertical = 8.dp)
             .fillMaxWidth()
+            .padding(horizontal = 14.dp, vertical = 8.dp)
             .clip(RoundedCornerShape(9.dp))
             .background(buttonColor)
-            .padding(vertical = 6.dp)
             .clickableRipple(onClick = onClick),
-        text = if (isUpdate) {
-            stringResource(R.string.whatsnew_btn_update)
-        } else {
-            stringResource(R.string.whatsnew_btn_install)
-        },
-        textAlign = TextAlign.Center,
-        style = LocalTypography.current.updateButton40,
-        color = if (isUpdate) {
-            LocalPalletV2.current.action.fwUpdate.text.onColor
-        } else {
-            LocalPalletV2.current.action.fwInstall.text.onColor
-        }
-    )
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.padding(vertical = 6.dp),
+            text = if (isUpdate) {
+                stringResource(R.string.whatsnew_btn_update)
+            } else {
+                stringResource(R.string.whatsnew_btn_install)
+            },
+            textAlign = TextAlign.Center,
+            style = LocalTypography.current.updateButton40,
+            color = if (isUpdate) {
+                LocalPalletV2.current.action.fwUpdate.text.onColor
+            } else {
+                LocalPalletV2.current.action.fwInstall.text.onColor
+            }
+        )
+    }
 }
 
 @Preview(
--- components/updater/card/src/main/java/com/flipperdevices/updater/card/composable/ComposableWhatsNew.kt
+++ components/updater/card/src/main/java/com/flipperdevices/updater/card/composable/ComposableWhatsNew.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,6 +48,7 @@ private fun ComposableWhatsNewButton(
 ) {
     Row(
         modifier = modifier
+            .clip(RoundedCornerShape(30.dp))
             .border(
                 width = 1.dp,
                 color = LocalPalletV2.current.action.neutral.border.tertiary.default,
