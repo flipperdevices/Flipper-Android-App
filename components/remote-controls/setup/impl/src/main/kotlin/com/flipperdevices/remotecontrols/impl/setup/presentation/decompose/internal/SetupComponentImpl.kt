80575bd7f38b86999562545dd95a88f7a03a0230
Nikita Kulikov <n@kulikov.uk>
2024-07-22 14:04:25 +0100

    Add example with ERROR_TYPE

--- components/remote-controls/setup/impl/src/main/kotlin/com/flipperdevices/remotecontrols/impl/setup/presentation/decompose/internal/SetupComponentImpl.kt
+++ components/remote-controls/setup/impl/src/main/kotlin/com/flipperdevices/remotecontrols/impl/setup/presentation/decompose/internal/SetupComponentImpl.kt
@@ -35,7 +35,7 @@ class SetupComponentImpl @AssistedInject constructor(
     @Assisted componentContext: ComponentContext,
     @Assisted override val param: SetupScreenDecomposeComponent.Param,
     @Assisted private val onBackClicked: DecomposeOnBackParameter,
-    @Assisted private val onIfrFileFound: (ifrFileId: Long) -> Unit,
+    @Assisted private val onIfrFileFound: ClassWhichNotExist,
     currentSignalViewModelFactory: CurrentSignalViewModel.Factory,
     createHistoryViewModel: Provider<HistoryViewModel>,
     createSaveTempSignalApi: Provider<SaveTempSignalApi>,
