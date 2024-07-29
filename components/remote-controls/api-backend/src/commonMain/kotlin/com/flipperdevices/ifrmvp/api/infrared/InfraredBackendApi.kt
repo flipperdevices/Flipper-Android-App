26c8e34e5d3f7b12c97271ed518580440e2126cc
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2024-07-29 13:13:31 +0000

    Replace ktorfit in remote controls (#905)
    **Background**
    
    Ktorfit updates sometimes causes difficulties to update it. This PR
    replace ktorfit inside remote-controls module with raw ktor
    
    **Changes**
    
    - Remote ktorfit from remote-controls 
    
    **Test plan**
    
    - Launch app
    - Open remote controls screen
    - Follow full userflow of this feature

--- components/remote-controls/api-backend/src/commonMain/kotlin/com/flipperdevices/ifrmvp/api/infrared/InfraredBackendApi.kt
+++ components/remote-controls/api-backend/src/commonMain/kotlin/com/flipperdevices/ifrmvp/api/infrared/InfraredBackendApi.kt
@@ -6,32 +6,15 @@ import com.flipperdevices.ifrmvp.backend.model.IfrFileContentResponse
 import com.flipperdevices.ifrmvp.backend.model.PagesLayoutBackendModel
 import com.flipperdevices.ifrmvp.backend.model.SignalRequestModel
 import com.flipperdevices.ifrmvp.backend.model.SignalResponseModel
-import de.jensklingenberg.ktorfit.http.Body
-import de.jensklingenberg.ktorfit.http.GET
-import de.jensklingenberg.ktorfit.http.POST
-import de.jensklingenberg.ktorfit.http.Query
 
 interface InfraredBackendApi {
-    @GET("categories")
     suspend fun getCategories(): CategoriesResponse
 
-    @GET("brands")
-    suspend fun getManufacturers(
-        @Query("category_id") categoryId: Long
-    ): BrandsResponse
+    suspend fun getManufacturers(categoryId: Long): BrandsResponse
 
-    @POST("signal")
-    suspend fun getSignal(
-        @Body request: SignalRequestModel
-    ): SignalResponseModel
+    suspend fun getSignal(request: SignalRequestModel): SignalResponseModel
 
-    @GET("key")
-    suspend fun getIfrFileContent(
-        @Query("ifr_file_id") ifrFileId: Long
-    ): IfrFileContentResponse
+    suspend fun getIfrFileContent(ifrFileId: Long): IfrFileContentResponse
 
-    @GET("ui")
-    suspend fun getUiFile(
-        @Query("ifr_file_id") ifrFileId: Long
-    ): PagesLayoutBackendModel
+    suspend fun getUiFile(ifrFileId: Long): PagesLayoutBackendModel
 }
