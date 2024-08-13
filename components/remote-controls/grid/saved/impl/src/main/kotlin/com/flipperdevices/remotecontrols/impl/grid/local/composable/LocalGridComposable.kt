97641100bbee67aef7dd7c0ebcc0e935b6087992
makeevrserg <makeevrserg@gmail.com>
2024-08-13 17:00:45 +0300

    add notification of syncing on grid ui

--- components/remote-controls/grid/saved/impl/src/main/kotlin/com/flipperdevices/remotecontrols/impl/grid/local/composable/LocalGridComposable.kt
+++ components/remote-controls/grid/saved/impl/src/main/kotlin/com/flipperdevices/remotecontrols/impl/grid/local/composable/LocalGridComposable.kt
@@ -1,5 +1,8 @@
 package com.flipperdevices.remotecontrols.impl.grid.local.composable
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
@@ -8,9 +11,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.flipperdevices.bridge.synchronization.api.SynchronizationState
 import com.flipperdevices.core.ui.theme.LocalPalletV2
 import com.flipperdevices.ifrmvp.core.ui.layout.shared.SharedTopBar
+import com.flipperdevices.remotecontrols.impl.grid.local.composable.components.ComposableSynchronizationNotification
 import com.flipperdevices.remotecontrols.impl.grid.local.composable.components.LocalGridComposableContent
 import com.flipperdevices.remotecontrols.impl.grid.local.presentation.decompose.LocalGridComponent
 
@@ -37,6 +44,18 @@ fun LocalGridComposable(
                 model = model,
                 modifier = Modifier.padding(scaffoldPaddings)
             )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .navigationBarsPadding()
+                    .padding(14.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                val state = (model as? LocalGridComponent.Model.Loaded)
+                    ?.synchronizationState
+                    ?: SynchronizationState.NotStarted
+                ComposableSynchronizationNotification(state)
+            }
         }
     )
 }
