0d345205a803c233a0ac2a9e65753b2ded902e82
Roman Makeev <makeevrserg@gmail.com>
2024-09-13 10:25:35 +0300

    fix detekt

--- components/toolstab/impl/src/main/java/com/flipperdevices/toolstab/impl/composable/elements/RemoteControlsComposable.kt
+++ components/toolstab/impl/src/main/java/com/flipperdevices/toolstab/impl/composable/elements/RemoteControlsComposable.kt
@@ -1,7 +1,6 @@
 package com.flipperdevices.toolstab.impl.composable.elements
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
