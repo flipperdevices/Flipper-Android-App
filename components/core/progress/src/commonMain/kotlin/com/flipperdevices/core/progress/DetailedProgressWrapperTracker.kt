bd66358dc885a25108ca75e1e3a6d2ae3bdaac02
Roman Makeev <makeevrserg@gmail.com>
2024-09-24 12:19:41 +0300

    move out progress listeners

--- components/core/progress/src/commonMain/kotlin/com/flipperdevices/core/progress/DetailedProgressWrapperTracker.kt
+++ components/core/progress/src/commonMain/kotlin/com/flipperdevices/core/progress/DetailedProgressWrapperTracker.kt
@@ -1,17 +1,17 @@
 package com.flipperdevices.core.progress
 
 import com.flipperdevices.core.buildkonfig.BuildKonfig
-import java.lang.Float.min
+import kotlin.math.min
 
 private const val MAX_PERCENT = 1.0f
 private const val MIN_PERCENT = 0f
 
-class ProgressWrapperTracker(
-    private val progressListener: ProgressListener,
+class DetailedProgressWrapperTracker(
+    private val progressListener: DetailedProgressListener,
     private val min: Float = MIN_PERCENT,
     private val max: Float = MAX_PERCENT
-) : ProgressListener, FixedProgressListener {
-    override suspend fun onProgress(current: Float) {
+) : DetailedProgressListener {
+    override suspend fun onProgress(current: Float, detail: DetailedProgressListener.Detail) {
         val diff = max - min
         if (diff <= 0) { // This means that our min and max are originally incorrect
             return
@@ -19,19 +19,25 @@ class ProgressWrapperTracker(
 
         val currentPercent = min + current * diff
 
-        progressListener.onProgress(min(min(currentPercent, max), MAX_PERCENT))
+        progressListener.onProgress(
+            min(
+                min(currentPercent, max),
+                MAX_PERCENT
+            ),
+            detail
+        )
     }
 
-    override suspend fun onProgress(current: Long, max: Long) {
+    suspend fun report(current: Long, max: Long, detail: DetailedProgressListener.Detail) {
         if (current > max) {
-            onProgress(MAX_PERCENT)
+            onProgress(MAX_PERCENT, detail)
             if (BuildKonfig.IS_LOG_ENABLED) {
                 error("Current larger then max (current: $current, max: $max)")
             }
             return
         }
         if (max == 0L) {
-            onProgress(MAX_PERCENT)
+            onProgress(MAX_PERCENT, detail)
             if (BuildKonfig.IS_LOG_ENABLED) {
                 error("Max is zero")
             }
@@ -39,6 +45,6 @@ class ProgressWrapperTracker(
         }
 
         val percent = current.toDouble() / max
-        onProgress(percent.toFloat())
+        onProgress(percent.toFloat(), detail)
     }
 }
--- components/core/progress/src/commonMain/kotlin/com/flipperdevices/core/progress/ProgressListener.kt
+++ components/core/progress/src/commonMain/kotlin/com/flipperdevices/core/progress/ProgressListener.kt
@@ -1,13 +1,5 @@
 package com.flipperdevices.core.progress
 
 fun interface ProgressListener {
-    interface Detail
-
     suspend fun onProgress(current: Float)
-}
-
-fun interface DetailedProgressListener {
-    interface Detail
-
-    suspend fun onProgress(current: Float, detail: Detail)
 }
