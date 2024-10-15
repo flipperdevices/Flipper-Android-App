3f81b0769ed13655aedd77101852d0198626829c
Roman Makeev <makeevrserg@gmail.com>
2024-10-15 11:33:29 +0300

    fix non-reactive feature in search, add debounceAfterFirst

--- components/filemngr/search/impl/src/androidMain/kotlin/com/flipperdevices/filemanager/search/impl/viewmodel/SearchViewModel.kt
+++ components/filemngr/search/impl/src/androidMain/kotlin/com/flipperdevices/filemanager/search/impl/viewmodel/SearchViewModel.kt
@@ -4,8 +4,10 @@ import com.flipperdevices.bridge.connection.feature.provider.api.FFeatureProvide
 import com.flipperdevices.bridge.connection.feature.provider.api.FFeatureStatus
 import com.flipperdevices.bridge.connection.feature.provider.api.get
 import com.flipperdevices.bridge.connection.feature.storage.api.FStorageFeatureApi
+import com.flipperdevices.bridge.connection.feature.storage.api.fm.FListingStorageApi
 import com.flipperdevices.bridge.connection.feature.storage.api.model.FileType
 import com.flipperdevices.bridge.connection.feature.storage.api.model.ListingItem
+import com.flipperdevices.core.ktx.jre.debounceAfterFirst
 import com.flipperdevices.core.ktx.jre.launchWithLock
 import com.flipperdevices.core.log.LogTagProvider
 import com.flipperdevices.core.ui.lifecycle.DecomposeViewModel
@@ -22,11 +24,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.drop
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
@@ -34,7 +32,6 @@ import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.flow.updateAndGet
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
@@ -59,70 +56,69 @@ class SearchViewModel @AssistedInject constructor(
     private val featureState = featureProvider.get<FStorageFeatureApi>()
         .stateIn(viewModelScope, SharingStarted.Eagerly, FFeatureStatus.Retrieving)
 
-    private suspend fun listingApiOrNull() = featureState
-        .filterIsInstance<FFeatureStatus.Supported<FStorageFeatureApi>>()
-        .firstOrNull()
-        ?.featureApi
-        ?.listingApi()
-
     fun onQueryChange(query: String) {
-        _searchState.updateAndGet { it.copy(query = query) }
+        _searchState.update { it.copy(query = query) }
     }
 
-    private suspend fun itemsFlow(path: Path): Flow<List<SearchItem>> {
-        return listingApiOrNull()
-            ?.lsFlow(path.toString())
-            ?.mapNotNull { items -> items.getOrNull() }
-            ?.map { items ->
-                items.map { item ->
-                    SearchItem(
-                        instance = item,
-                        fullPath = path.resolve(item.fileName)
-                    )
-                }
-            } ?: emptyFlow()
-    }
+    private suspend fun itemsFlow(
+        listingApi: FListingStorageApi,
+        path: Path
+    ): Flow<List<SearchItem>> = listingApi
+        .lsFlow(path.toString())
+        .mapNotNull { items -> items.getOrNull() }
+        .map { items ->
+            items.map { item ->
+                SearchItem(
+                    instance = item,
+                    fullPath = path.resolve(item.fileName)
+                )
+            }
+        }
 
     private fun itemsFlowRecursive(
+        listingApi: FListingStorageApi,
         path: Path
     ): Flow<List<SearchItem>> = flow {
-        itemsFlow(path)
+        itemsFlow(listingApi, path)
             .onEach { searchItems -> emit(searchItems) }
             .onEach { searchItems ->
                 searchItems
                     .filter { searchItem -> searchItem.instance.fileType == FileType.DIR }
                     .forEach { searchItem ->
-                        itemsFlowRecursive(searchItem.fullPath)
+                        itemsFlowRecursive(listingApi, searchItem.fullPath)
                             .onEach { searchItems -> emit(searchItems) }
                             .collect()
                     }
             }.collect()
     }
 
-    private fun trySearch() {
-        val query = queryState.value.query
+    private fun trySearch(listingApi: FListingStorageApi) {
         viewModelScope.launch {
             lastSearchJob?.cancelAndJoin()
             launchWithLock(mutex, viewModelScope, "search") {
                 lastSearchJob = coroutineContext.job
-                _state.emit(State.Loading)
-                itemsFlowRecursive(path)
-                    .onEach {
-                        val filteredItems = it.filter { item ->
-                            if (query.isEmpty()) {
-                                true
-                            } else {
-                                item.fullPath.name.contains(query, ignoreCase = true)
-                            }
+                val query = queryState.value.query
+                _state.emit(State.Loaded(path = path, isSearching = true))
+                val itemsFlow = when {
+                    query.isEmpty() -> itemsFlow(listingApi, path)
+                    else -> itemsFlowRecursive(listingApi, path)
+                }
+                itemsFlow.onEach {
+                    val filteredItems = it.filter { item ->
+                        if (query.isEmpty()) {
+                            true
+                        } else {
+                            item.fullPath.name.contains(query, ignoreCase = true)
                         }
-                        _state.update { oldState ->
-                            val state = (oldState as? State.Loaded) ?: State.Loaded(
-                                isSearching = true,
-                                path = path
-                            )
-                            state.copy(items = state.items.plus(filteredItems).toImmutableList())
-                        }
-                    }.collect()
+                    }
+                    _state.update { oldState ->
+                        val state = (oldState as? State.Loaded) ?: State.Loaded(
+                            isSearching = true,
+                            path = path
+                        )
+                        state.copy(items = state.items.plus(filteredItems).toImmutableList())
+                    }
+                }.collect()
                 _state.update { oldState ->
                     val state = (oldState as? State.Loaded) ?: State.Loaded(path = path)
                     state.copy(isSearching = false)
@@ -132,21 +128,18 @@ class SearchViewModel @AssistedInject constructor(
     }
 
     init {
-        featureState
-            .onEach { status ->
-                when (status) {
+        combine(
+            flow = featureState,
+            flow2 = _searchState.debounceAfterFirst(timeout = 400.milliseconds),
+            transform = { featureState, _ ->
+                when (featureState) {
                     FFeatureStatus.NotFound -> _state.emit(State.Unsupported)
                     FFeatureStatus.Retrieving -> _state.emit(State.Loading)
                     FFeatureStatus.Unsupported -> _state.emit(State.Unsupported)
-                    is FFeatureStatus.Supported -> trySearch()
+                    is FFeatureStatus.Supported -> trySearch(featureState.featureApi.listingApi())
                 }
-            }.launchIn(viewModelScope)
-        // drop initial element
-        _searchState
-            .drop(count = 1)
-            .debounce(timeout = 400.milliseconds)
-            .onEach { trySearch() }
-            .launchIn(viewModelScope)
+            }
+        ).launchIn(viewModelScope)
     }
 
     data class SearchState(
