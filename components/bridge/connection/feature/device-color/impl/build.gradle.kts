8917829bea1b70b4ef1de5ab2fececad096ec8b5
Roman Makeev <makeevrserg@gmail.com>
2024-12-10 17:21:58 +0300

    add flipper version check for device color get request

--- components/bridge/connection/feature/device-color/impl/build.gradle.kts
+++ components/bridge/connection/feature/device-color/impl/build.gradle.kts
@@ -13,12 +13,14 @@ commonDependencies {
     implementation(projects.components.core.log)
     implementation(projects.components.core.ktx)
     implementation(projects.components.core.preference)
+    implementation(projects.components.core.data)
 
     implementation(projects.components.bridge.connection.feature.common.api)
     implementation(projects.components.bridge.connection.transport.common.api)
     implementation(projects.components.bridge.connection.feature.rpc.api)
     implementation(projects.components.bridge.connection.feature.rpc.model)
     implementation(projects.components.bridge.connection.feature.rpcinfo.api)
+    implementation(projects.components.bridge.connection.feature.protocolversion.api)
 
     implementation(projects.components.bridge.connection.pbutils)
 
