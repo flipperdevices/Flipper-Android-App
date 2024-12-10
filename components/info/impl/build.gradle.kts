ad70c93d3208eca2f84ef149f6374d3ffcad78b4
Roman Makeev <makeevrserg@gmail.com>
2024-12-10 17:28:29 +0300

    firmware update

--- components/info/impl/build.gradle.kts
+++ components/info/impl/build.gradle.kts
@@ -47,11 +47,12 @@ dependencies {
     implementation(projects.components.bridge.connection.feature.storageinfo.api)
     implementation(projects.components.bridge.connection.feature.alarm.api)
     implementation(projects.components.bridge.connection.feature.deviceColor.api)
+    implementation(projects.components.bridge.connection.feature.protocolversion.api)
 
-    implementation(projects.components.bridge.api)
-    implementation(projects.components.bridge.service.api)
-    implementation(projects.components.bridge.pbutils)
-    implementation(projects.components.bridge.rpcinfo.api)
+//    implementation(projects.components.bridge.api)
+//    implementation(projects.components.bridge.service.api)
+//    implementation(projects.components.bridge.pbutils)
+//    implementation(projects.components.bridge.rpcinfo.api)
 
     // Core deps
     implementation(libs.ktx)
