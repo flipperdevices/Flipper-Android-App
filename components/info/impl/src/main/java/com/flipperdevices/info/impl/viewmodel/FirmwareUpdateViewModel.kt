d17f514f188d624fbd8fe3e22618f076ee557bf8
Roman Makeev <makeevrserg@gmail.com>
2024-12-10 17:28:29 +0300

    firmware update

--- components/info/impl/src/main/java/com/flipperdevices/info/impl/viewmodel/FirmwareUpdateViewModel.kt
+++ components/info/impl/src/main/java/com/flipperdevices/info/impl/viewmodel/FirmwareUpdateViewModel.kt
@@ -1,40 +1,38 @@
 package com.flipperdevices.info.impl.viewmodel
 
-import com.flipperdevices.bridge.api.manager.ktx.state.ConnectionState
-import com.flipperdevices.bridge.api.manager.ktx.state.FlipperSupportedState
-import com.flipperdevices.bridge.service.api.FlipperServiceApi
-import com.flipperdevices.bridge.service.api.provider.FlipperBleServiceConsumer
-import com.flipperdevices.bridge.service.api.provider.FlipperServiceProvider
+import com.flipperdevices.bridge.connection.feature.protocolversion.api.FVersionFeatureApi
+import com.flipperdevices.bridge.connection.feature.protocolversion.model.FlipperSupportedState
+import com.flipperdevices.bridge.connection.feature.provider.api.FFeatureProvider
+import com.flipperdevices.bridge.connection.feature.provider.api.FFeatureStatus
+import com.flipperdevices.bridge.connection.feature.provider.api.get
 import com.flipperdevices.core.log.LogTagProvider
-import com.flipperdevices.core.log.info
 import com.flipperdevices.core.ui.lifecycle.DecomposeViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 class FirmwareUpdateViewModel @Inject constructor(
-    serviceProvider: FlipperServiceProvider
-) : DecomposeViewModel(), FlipperBleServiceConsumer, LogTagProvider {
+    private val fFeatureProvider: FFeatureProvider
+) : DecomposeViewModel(), LogTagProvider {
     override val TAG = "FirmwareUpdateViewModel"
 
-    private val connectionState = MutableStateFlow(
-        FlipperSupportedState.READY
-    )
-
-    init {
-        serviceProvider.provideServiceApi(consumer = this, lifecycleOwner = this)
-    }
-
-    fun getState(): StateFlow<FlipperSupportedState> = connectionState
-
-    override fun onServiceApiReady(serviceApi: FlipperServiceApi) {
-        serviceApi.connectionInformationApi.getConnectionStateFlow().onEach {
-            info { "Receive connection state: $it" }
-            if (it is ConnectionState.Ready) {
-                connectionState.emit(it.supportedState)
+    fun getState(): StateFlow<FlipperSupportedState> {
+        return fFeatureProvider.get<FVersionFeatureApi>().flatMapLatest { status ->
+            when (status) {
+                FFeatureStatus.NotFound -> flowOf(FlipperSupportedState.DEPRECATED_FLIPPER)
+                FFeatureStatus.Unsupported -> flowOf(FlipperSupportedState.DEPRECATED_FLIPPER)
+                FFeatureStatus.Retrieving -> flowOf(FlipperSupportedState.READY)
+                is FFeatureStatus.Supported -> {
+                    status.featureApi
+                        .getSupportedStateFlow()
+                        .map { state -> state ?: FlipperSupportedState.DEPRECATED_FLIPPER }
+                }
             }
-        }.launchIn(viewModelScope)
+        }.stateIn(viewModelScope, SharingStarted.Eagerly, FlipperSupportedState.READY)
     }
+
 }
