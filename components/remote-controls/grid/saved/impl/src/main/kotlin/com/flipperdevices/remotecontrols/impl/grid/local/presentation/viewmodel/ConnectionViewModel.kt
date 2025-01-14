6229154e29cd192a4e9b6f59e5fbe8629321d68d
Roman Makeev <makeevrserg@gmail.com>
2025-01-14 17:38:29 +0300

    fix detekt

--- components/remote-controls/grid/saved/impl/src/main/kotlin/com/flipperdevices/remotecontrols/impl/grid/local/presentation/viewmodel/ConnectionViewModel.kt
+++ components/remote-controls/grid/saved/impl/src/main/kotlin/com/flipperdevices/remotecontrols/impl/grid/local/presentation/viewmodel/ConnectionViewModel.kt
@@ -1,10 +1,8 @@
 package com.flipperdevices.remotecontrols.impl.grid.local.presentation.viewmodel
 
-import com.flipperdevices.bridge.service.api.provider.FlipperServiceProvider
 import com.flipperdevices.core.ui.lifecycle.DecomposeViewModel
 import com.flipperdevices.infrared.api.InfraredConnectionApi
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
