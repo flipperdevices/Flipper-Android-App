ff84bb0588c18e086f88635e75bcd0d2f0dee548
Roman Makeev <makeevrserg@gmail.com>
2025-01-15 13:25:02 +0300

    migrate stresstest to new api

--- components/debug/stresstest/src/main/java/com/flipperdevices/debug/stresstest/viewmodel/StressTestViewModel.kt
+++ components/debug/stresstest/src/main/java/com/flipperdevices/debug/stresstest/viewmodel/StressTestViewModel.kt
@@ -1,53 +1,51 @@
 package com.flipperdevices.debug.stresstest.viewmodel
 
 import androidx.compose.ui.graphics.Color
-import com.flipperdevices.bridge.api.manager.FlipperRequestApi
-import com.flipperdevices.bridge.api.manager.delegates.toHumanReadableString
-import com.flipperdevices.bridge.api.model.FlipperRequestPriority
-import com.flipperdevices.bridge.api.model.FlipperSerialSpeed
-import com.flipperdevices.bridge.api.model.wrapToRequest
-import com.flipperdevices.bridge.protobuf.ProtobufConstants
-import com.flipperdevices.bridge.service.api.FlipperServiceApi
-import com.flipperdevices.bridge.service.api.provider.FlipperServiceProvider
+import com.flipperdevices.bridge.connection.feature.provider.api.FFeatureProvider
+import com.flipperdevices.bridge.connection.feature.provider.api.FFeatureStatus
+import com.flipperdevices.bridge.connection.feature.provider.api.get
+import com.flipperdevices.bridge.connection.feature.provider.api.getSync
+import com.flipperdevices.bridge.connection.feature.serialspeed.api.FSpeedFeatureApi
+import com.flipperdevices.bridge.connection.feature.storage.api.FStorageFeatureApi
+import com.flipperdevices.bridge.connection.feature.storage.api.fm.FFileDownloadApi
+import com.flipperdevices.bridge.connection.orchestrator.api.FDeviceOrchestrator
+import com.flipperdevices.bridge.connection.orchestrator.api.model.FDeviceConnectStatus
+import com.flipperdevices.bridge.connection.transport.common.api.serial.FlipperSerialSpeed
 import com.flipperdevices.core.ktx.jre.FlipperDispatchers
-import com.flipperdevices.core.ktx.jre.split
+import com.flipperdevices.core.progress.copyWithProgress
 import com.flipperdevices.core.ui.lifecycle.DecomposeViewModel
 import com.flipperdevices.debug.stresstest.model.LogLine
 import com.flipperdevices.debug.stresstest.model.StressTestState
-import com.flipperdevices.protobuf.main
-import com.flipperdevices.protobuf.storage.deleteRequest
-import com.flipperdevices.protobuf.storage.file
-import com.flipperdevices.protobuf.storage.readRequest
-import com.flipperdevices.protobuf.storage.writeRequest
-import com.google.protobuf.ByteString
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.nio.ByteBuffer
-import java.util.Arrays
+import okio.buffer
+import okio.source
+import kotlin.random.Random
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
-import kotlin.random.Random
 
 const val BUFFER_SIZE = 20 * 1024
 const val TEST_FILE = "/ext/stresstest_mobile.tmp"
 
 @Suppress("TooManyFunctions")
 class StressTestViewModel @Inject constructor(
-    private val serviceProvider: FlipperServiceProvider
+    private val fFeatureProvider: FFeatureProvider,
+    private val fDeviceOrchestrator: FDeviceOrchestrator
 ) : DecomposeViewModel() {
     private val isStressTestRunning = AtomicBoolean(false)
     private val debugLog = MutableStateFlow(persistentListOf<LogLine>())
@@ -57,29 +55,25 @@ class StressTestViewModel @Inject constructor(
     private var stressTestJob: Job? = null
 
     init {
-        serviceProvider.provideServiceApi(this) {
-            subscribeToConnectionStateUpdate(it)
-            subscribeToSpeedStateUpdate(it)
-        }
+        subscribeToConnectionStateUpdate()
+        subscribeToSpeedStateUpdate()
     }
 
     fun startBruteforce() {
-        serviceProvider.provideServiceApi(this) { serviceApi ->
-            stressTestJob = viewModelScope.launch {
-                if (!isStressTestRunning.compareAndSet(false, true)) {
-                    return@launch
-                }
-                writeToLog("Stress test starting")
-
-                while (isActive && isStressTestRunning.get()) {
-                    removeTemporaryFile(serviceApi.requestApi)
-                    fillBuffer()
-                    sendBufferToFile(serviceApi.requestApi)
-                    receiveBufferFromFileAndCheck(serviceApi.requestApi)
-                }
-
-                writeToLog("Stress test stopping")
+        stressTestJob = viewModelScope.launch {
+            if (!isStressTestRunning.compareAndSet(false, true)) {
+                return@launch
             }
+            writeToLog("Stress test starting")
+
+            while (isActive && isStressTestRunning.get()) {
+                removeTemporaryFile()
+                fillBuffer()
+                sendBufferToFile()
+                receiveBufferFromFileAndCheck()
+            }
+
+            writeToLog("Stress test stopping")
         }
     }
 
@@ -102,17 +96,16 @@ class StressTestViewModel @Inject constructor(
         }
     }
 
-    private suspend fun removeTemporaryFile(
-        requestApi: FlipperRequestApi
-    ) = withContext(FlipperDispatchers.workStealingDispatcher) {
-        requestApi.request(
-            main {
-                storageDeleteRequest = deleteRequest {
-                    path = TEST_FILE
-                }
-            }.wrapToRequest()
-        ).single()
-        writeToLog("Remove $TEST_FILE")
+    private suspend fun removeTemporaryFile() {
+        withContext(FlipperDispatchers.workStealingDispatcher) {
+            val fStorageFeatureApi = fFeatureProvider
+                .getSync<FStorageFeatureApi>()
+                ?: return@withContext
+            fStorageFeatureApi.deleteApi()
+                .delete(TEST_FILE)
+                .onFailure { writeToLog("Could not delete $TEST_FILE") }
+                .onSuccess { writeToLog("Remove $TEST_FILE") }
+        }
     }
 
     private suspend fun fillBuffer() = withContext(FlipperDispatchers.workStealingDispatcher) {
@@ -120,56 +113,42 @@ class StressTestViewModel @Inject constructor(
     }
 
     private suspend fun sendBufferToFile(
-        requestApi: FlipperRequestApi
     ) = withContext(FlipperDispatchers.workStealingDispatcher) {
-        val splittedBytes = byteBuffer
-            .split(ProtobufConstants.MAX_FILE_DATA)
-        val requests = splittedBytes
-            .mapIndexed { index, bytes ->
-                getWriteRequest(bytes, isLast = index == splittedBytes.lastIndex)
+        val fStorageFeatureApi = fFeatureProvider
+            .getSync<FStorageFeatureApi>()
+            ?: return@withContext
+
+        byteBuffer.inputStream().source().buffer().use { source ->
+            fStorageFeatureApi.uploadApi().sink(TEST_FILE).use { sink ->
+                source.copyWithProgress(
+                    sink,
+                    { _, _ -> },
+                    sourceLength = { byteBuffer.size.toLong() }
+                )
             }
-            .asFlow()
-        requestApi.request(requests, onCancel = { id ->
-            requestApi.request(
-                main {
-                    hasNext = false
-                    commandId = id
-                    storageWriteRequest = writeRequest {
-                        path = TEST_FILE
-                    }
-                }.wrapToRequest(FlipperRequestPriority.RIGHT_NOW)
-            ).collect()
-        })
+        }
+
         writeToLog("Write file with length $BUFFER_SIZE successfully")
     }
 
-    private fun getWriteRequest(bytes: ByteArray, isLast: Boolean) = main {
-        hasNext = !isLast
-        storageWriteRequest = writeRequest {
-            path = TEST_FILE
-            file = file { data = ByteString.copyFrom(bytes) }
-        }
-    }.wrapToRequest()
+
+    suspend fun FFileDownloadApi.readByteArray(pathOnFlipper: String) = coroutineScope {
+        source(pathOnFlipper, this)
+            .buffer()
+            .readByteArray()
+    }
 
     private suspend fun receiveBufferFromFileAndCheck(
-        requestApi: FlipperRequestApi
     ) = withContext(FlipperDispatchers.workStealingDispatcher) {
-        val receiveBytes = ByteBuffer.allocate(byteBuffer.size)
-        var bytesCount = 0L
-        requestApi.request(
-            main {
-                storageReadRequest = readRequest {
-                    path = TEST_FILE
-                }
-            }.wrapToRequest()
-        ).collect {
-            val receivedBytesArray = it.storageReadResponse.file.data.toByteArray()
-            bytesCount += receivedBytesArray.size
-            receiveBytes.put(receivedBytesArray)
-        }
-        writeToLog("Read file with length $bytesCount successfully")
+        val fStorageFeatureApi = fFeatureProvider
+            .getSync<FStorageFeatureApi>()
+            ?: return@withContext
 
-        val isEquals = Arrays.equals(receiveBytes.array(), byteBuffer)
+        val isEquals = fStorageFeatureApi.downloadApi()
+            .readByteArray(TEST_FILE)
+            .also { writeToLog("Read file with length ${it.size} successfully") }
+            .contentEquals(byteBuffer)
+
         if (isEquals) {
             writeToLog("Buffer equals!", Color.Green)
             stressTestState.update {
@@ -183,17 +162,28 @@ class StressTestViewModel @Inject constructor(
         }
     }
 
-    private fun subscribeToConnectionStateUpdate(serviceApi: FlipperServiceApi) {
-        serviceApi.connectionInformationApi.getConnectionStateFlow().onEach {
-            writeToLog("Connection state is ${it.toHumanReadableString()}")
-        }.launchIn(viewModelScope)
+    private fun FDeviceConnectStatus.toHumanReadableName(): String {
+        return when (this) {
+            is FDeviceConnectStatus.Connected -> "Connected-${this.device.humanReadableName}"
+            is FDeviceConnectStatus.Connecting -> "Connecting-${this.device.humanReadableName}"
+            is FDeviceConnectStatus.Disconnected -> "Disconnected"
+            is FDeviceConnectStatus.Disconnecting -> "Disconnecting"
+        }
     }
 
-    private fun subscribeToSpeedStateUpdate(
-        serviceApi: FlipperServiceApi
-    ) = viewModelScope.launch {
-        serviceApi.requestApi.getSpeed().onEach {
-            speedState.emit(it)
-        }.launchIn(viewModelScope)
+    private fun subscribeToConnectionStateUpdate() {
+        fDeviceOrchestrator
+            .getState()
+            .onEach { writeToLog("Connection state is ${it.toHumanReadableName()}") }
+            .launchIn(viewModelScope)
+    }
+
+    private fun subscribeToSpeedStateUpdate() = viewModelScope.launch {
+        fFeatureProvider.get<FSpeedFeatureApi>()
+            .map { status -> status as? FFeatureStatus.Supported<FSpeedFeatureApi> }
+            .map { status -> status?.featureApi }
+            .flatMapLatest { feature -> feature?.getSpeed() ?: flowOf(null) }
+            .onEach { speed -> speedState.emit(speed ?: FlipperSerialSpeed()) }
+            .launchIn(viewModelScope)
     }
 }
