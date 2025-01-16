90ff4054e88f91bc9f9e2e141a8bba11d51dc0c5
Roman Makeev <makeevrserg@gmail.com>
2025-01-16 09:42:59 +0300

    fix android sample build

--- components/bridge/connection/sample/android/src/androidMain/kotlin/com/flipperdevices/bridge/connection/di/AndroidAppComponent.kt
+++ components/bridge/connection/sample/android/src/androidMain/kotlin/com/flipperdevices/bridge/connection/di/AndroidAppComponent.kt
@@ -1,5 +1,13 @@
 package com.flipperdevices.bridge.connection.di
 
+import android.app.Application
+import android.content.Context
+import com.flipperdevices.core.di.AppGraph
+import com.flipperdevices.core.di.ApplicationParams
+import com.squareup.anvil.annotations.MergeComponent
+import dagger.BindsInstance
+import javax.inject.Singleton
+
 @Singleton
 @MergeComponent(AppGraph::class)
 interface AndroidAppComponent : AppComponent {
